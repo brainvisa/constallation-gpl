c131d3e6ba3691c0666c6c8544d4721695f90775
denisri <denis.riviere.gm@gmail.com>
2013-05-07 13:10:09 +0000

    connectivity matrix viewer
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@55609 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ brainvisa/toolboxes/constellation/processes/viewers/anatomistViewConnmatrix.py
@@ -0,0 +1,47 @@
+from brainvisa.processes import *
+try:
+    from brainvisa import anatomist as ana
+except:
+    pass
+
+def validation():
+    try:
+        from brainvisa import anatomist as ana
+    except:
+        raise ValidationError( _t_( 'Anatomist not available' ) )
+
+
+name = 'Anatomist view connectivity matrix'
+roles = ( 'viewer', )
+
+
+signature = Signature(
+    'connectivity_matrix',
+        ReadDiskItem( 'Gyrus connectivity matrix', 'Matrix sparse' ),
+    'white_mesh', ReadDiskItem( 'AimsBothWhite', 'anatomist mesh formats' ),
+    'gyrus_texture',
+        ReadDiskItem( 'Label texture', 'anatomist texture formats' ),
+)
+
+
+def initialization( self ):
+    self.linkParameters( 'white_mesh', 'connectivity_matrix' )
+    self.linkParameters( 'gyrus_texture', 'connectivity_matrix' )
+
+
+def execution( self, context ):
+    a = ana.Anatomist()
+    mesh = a.loadObject( self.white_mesh )
+    patch = a.loadObject( self.gyrus_texture )
+    sparse = a.loadObject( self.connectivity_matrix )
+    conn = a.fusionObjects( [ mesh, patch, sparse ],
+        method='ConnectivityMatrixFusionMethod' )
+    if conn is None:
+        raise ValueError( 'could not fusion objects - matrix, mesh and ' \
+            'labels texture may not correspond.' )
+    win = a.createWindow( '3D' )
+    win.addObjects( conn )
+    win.setControl( 'ConnectivityMatrixControl' )
+
+    return [ win, conn ]
+
