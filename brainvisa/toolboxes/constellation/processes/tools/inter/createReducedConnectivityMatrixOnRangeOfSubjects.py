130cd37bc4d1613562cd1fe5049201dcaf66fc45
denisri <denis.riviere.gm@gmail.com>
2013-06-04 17:03:50 +0000

    reduced connectivity matrix for all subjects in a group, transformed in to a dynamic pipeline, allowing distributed execution Some links still don't work (as before) for an unknown reason
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@56248 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/tools/inter/createReducedConnectivityMatrixOnRangeOfSubjects.py
+++ brainvisa/toolboxes/constellation/processes/tools/inter/createReducedConnectivityMatrixOnRangeOfSubjects.py
@@ -3,6 +3,7 @@ from brainvisa.processes import *
 from soma.path import find_in_path
 from brainvisa.group_utils import Subject
 from soma.minf.api import registerClass, readMinf
+from soma.functiontools import partial
 
 def validation():
   if not find_in_path( 'constelConnectionDensityTexture' ):
@@ -22,9 +23,35 @@ signature = Signature(
               'average_mesh', ReadDiskItem( 'BothAverageBrainWhite', 'BrainVISA mesh formats' ),
          'gyri_segmentation', ListOf( ReadDiskItem( 'BothResampledGyri', 'Aims texture formats' ) ),
               'vertex_index', ListOf( ReadDiskItem( 'Vertex Index', 'Text file' ) ),
-                
   'connectivity_matrix_reduced', ListOf( WriteDiskItem( 'Group Reduced Connectivity Matrix', 'GIS image' ) ),
 )
+
+
+def afterChildAddedCallback( self, parent, key, child ):
+  # Set default values
+  child.removeLink( 'filtered_watershed', 'connectivity_matrix_full' )
+  child.removeLink( 'connectivity_matrix_reduced', 'filtered_watershed' )
+  child.removeLink( 'vertex_index', 'connectivity_matrix_full' )
+
+  child.signature[ 'filtered_watershed' ] \
+    = parent.signature[ 'filtered_watershed' ]
+  child.signature[ 'white_mesh' ] = parent.signature[ 'average_mesh' ]
+
+  child.patch_label = parent.patch_label
+  child.filtered_watershed = parent.filtered_watershed
+  child.white_mesh = parent.average_mesh
+
+  ## Add link between eNode.ListOf_Input_3dImage and pNode.Input_3dImage
+  parent.addLink( key + '.filtered_watershed', 'filtered_watershed' )
+  parent.addLink( key + '.patch_label', 'patch_label' )
+  parent.addLink( key + '.white_mesh', 'average_mesh' )
+
+
+def beforeChildRemovedCallback( self, parent, key, child ):
+  parent.removeLink( key + '.filtered_watershed', 'filtered_watershed' )
+  parent.removeLink( key + '.patch_label', 'patch_label' )
+  parent.removeLink( key + '.white_mesh', 'average_mesh' )
+
 
 def initialization ( self ):
   def linkIndividualProfiles( self, dummy ):
@@ -51,52 +78,74 @@ def initialization ( self ):
         atts[ 'subject' ] = p.get( 'subject' )
         profiles.append( WriteDiskItem( 'Group Reduced Connectivity Matrix', 'GIS image' ).findValue( atts ) )
       return profiles
+  def linkWatershed( self, dummy ):
+    if self.group is not None and self.patch_label and self.texture_out \
+        and self.study_name:
+      atts = self.group.hierarchyAttributes()
+      atts[ 'texture' ] = self.texture_out
+      atts[ 'gyrus' ] = 'G' + str(self.patch_label)
+      atts[ 'study' ] = self.study_name
+      #print 'atts:', atts
+      return ReadDiskItem( 'Averaged Filtered Watershed', 'Aims texture formats' ).findValue( atts )
+
   self.linkParameters( 'individual_matrix_sparse', ( 'group', 'study_name', 'texture_in', 'patch_label' ), linkIndividualProfiles )
-  self.linkParameters( 'filtered_watershed', ( 'group', 'patch_label', 'texture_out' ) )
+  self.linkParameters( 'filtered_watershed', ( 'group', 'patch_label', 'texture_out', 'study_name' ), linkWatershed )
   self.linkParameters( 'connectivity_matrix_reduced', ( 'group','individual_matrix_sparse', 'texture_out') , linkProfiles )
   self.linkParameters( 'vertex_index', 'individual_matrix_sparse')
   self.signature['individual_matrix_sparse'].userLevel = 2
 
-def execution ( self, context ):
-  context.write( 'starting step 3.8 : Connectivity matrix to targets from matrix... ' )
-  '''
-  Connectivity Matrices reduction according to a parcellated white mesh texture.
-  The reduced connectivity profiles of all subjects are normalized using L2 norm.
-  '''
-  registerClass('minf_2.0', Subject, 'Subject')
-  groupOfSubjects = readMinf(self.group.fullPath())
+  eNode = ParallelExecutionNode( 'Reduced_connectivity_matrix',
+                                 parameterized = self,
+                                 possibleChildrenProcesses = [ 'createReducedConnectivityMatrix' ],
+                                 notify = True )
+  self.setExecutionNode( eNode )
 
-  context.write( 'Reducing connectivity matrices.' )
-  
-  listReducedMatrix = []
-  n = 0
-  g = 0
-  for matrix in self.individual_matrix_sparse:
-    print '--------------------------------'
-    print n
-    print matrix
-    print self.vertex_index[n]
-    print self.gyri_segmentation[g]
-    print self.connectivity_matrix_reduced[n]
-    print '--------------------------------'
-    context.system( "constelConnectionDensityTexture",
-      '-mesh', self.average_mesh,
-      '-connmatrixfile', matrix,
-      '-connfmt', 'binar_sparse',
-      '-targetregionstex', self.filtered_watershed,
-      '-seedregionstex', self.gyri_segmentation[g],
-      '-seedlabel', self.patch_label,
-      '-connmatrix', self.connectivity_matrix_reduced[n],
-      '-type', 'seedVertex_to_targets',
-      '-seedvertexindex', self.vertex_index[n],
-      '-normalize', 1,
-    )
-    listReducedMatrix.append( self.connectivity_matrix_reduced[n] )
-    n += 1
-    if len(self.gyri_segmentation) == 1:
-      g = 0
-    else:
-      g += 1
-  listReducedMatrix = self.connectivity_matrix_reduced
-  context.write( 'OK' )
+  # Add callback to warn about child add and remove
+  eNode.afterChildAdded.add(\
+    ExecutionNode.MethodCallbackProxy( self.afterChildAddedCallback ) )
+  eNode.beforeChildRemoved.add(\
+    ExecutionNode.MethodCallbackProxy( self.beforeChildRemovedCallback ) )
+  #eNode.afterChildRemoved.add(\
+    #ExecutionNode.MethodCallbackProxy( self.afterChildRemovedCallback ) )
+
+  # Add links to refresh child nodes when main lists are modified
+  eNode.addLink( None,
+                 'individual_matrix_sparse',
+                 partial( brainvisa.processes.mapValuesToChildrenParameters,
+                          eNode,
+                          eNode,
+                          'connectivity_matrix_full',
+                          'individual_matrix_sparse',
+                          defaultProcess = 'createReducedConnectivityMatrix',
+                          name='createReducedConnectivityMatrix' ) )
+
+  eNode.addLink( None,
+                 'connectivity_matrix_reduced',
+                 partial( brainvisa.processes.mapValuesToChildrenParameters,
+                          eNode,
+                          eNode,
+                          'connectivity_matrix_reduced',
+                          'connectivity_matrix_reduced',
+                          defaultProcess = 'createReducedConnectivityMatrix',
+                          name='createReducedConnectivityMatrix' ) )
+
+  eNode.addLink( None,
+                 'vertex_index',
+                 partial( brainvisa.processes.mapValuesToChildrenParameters,
+                          eNode,
+                          eNode,
+                          'vertex_index',
+                          'vertex_index',
+                          defaultProcess = 'createReducedConnectivityMatrix',
+                          name='createReducedConnectivityMatrix' ) )
+
+  eNode.addLink( None,
+                 'gyri_segmentation',
+                 partial( brainvisa.processes.mapValuesToChildrenParameters,
+                          eNode,
+                          eNode,
+                          'gyri_segmentation',
+                          'gyri_segmentation',
+                          defaultProcess = 'createReducedConnectivityMatrix',
+                          name='createReducedConnectivityMatrix' ) )
 
