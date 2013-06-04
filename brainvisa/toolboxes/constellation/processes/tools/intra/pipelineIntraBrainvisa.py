a48eba0cc67860344920c84a92a98e03912fd438
lefranc <lefranc@bioproj.invalid>
2013-06-04 15:55:47 +0000

    The first step were replaced with an other process : bundlesFiltering
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@56242 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/tools/intra/pipelineIntraBrainvisa.py
+++ brainvisa/toolboxes/constellation/processes/tools/intra/pipelineIntraBrainvisa.py
@@ -11,69 +11,49 @@ name = 'Brainvisa Intra pipeline'
 userLevel = 2
 
 signature = Signature(
-  'reorganized_subset_of_tract', ReadDiskItem( 'Gyri Regrouped Subset of Tracts', 'Aims bundles' ),
-            'gyri_segmentation', ReadDiskItem( 'BothResampledGyri', 'Aims texture formats' ),
-                   'white_mesh', ReadDiskItem( 'AimsBothWhite', 'Aims mesh formats' ),
-       'diff_to_anat_transform', ReadDiskItem( 'Transformation matrix', 'Transformation matrix' ),
-                  'patch_label', Integer(),
-                       'smooth', Float(),
+  #'reorganized_subset_of_tract', ReadDiskItem( 'Gyri Regrouped Subset of Tracts', 'Aims bundles' ),
+            'study', String(),
+          'texture', String(),
+            'gyrus', String(),
+          'subject', ReadDiskItem( 'subject', 'directory' ),
+  'subset_of_tract', ReadDiskItem( 'Fascicles bundles', 'Aims bundles' ),
+     'gyri_texture', ReadDiskItem( 'BothResampledGyri', 'Aims texture formats' ),
+       'white_mesh', ReadDiskItem( 'AimsBothWhite', 'Aims mesh formats' ),
+         'dw_to_t1', ReadDiskItem( 'Transformation matrix', 'Transformation matrix' ),
+        'smoothing', Float(),
 )
 
 def initialization( self ):
 
   eNode = SerialExecutionNode( self.name, parameterized=self )
 
-  ### 02/03 - Filtering per gyrus
-  eNode.addChild( 'PrepareTracts',
-                  ProcessExecutionNode( 'filteringPerGyrus',
+  ### Bundles Fitering
+  eNode.addChild( 'Filter',
+                  ProcessExecutionNode( 'bundlesFiltering',
                   optional = 1 ) )
 
-  eNode.addDoubleLink( 'PrepareTracts.reorganized_subset_of_tract',
-                       'reorganized_subset_of_tract' )
-                       
-  eNode.addDoubleLink( 'PrepareTracts.gyri_segmentation',
-                       'gyri_segmentation' )
-                       
-  eNode.addDoubleLink( 'PrepareTracts.patch_label',
-                       'patch_label' )
+  eNode.addDoubleLink( 'Filter.study',
+                       'study' )
+  eNode.addDoubleLink( 'Filter.texture',
+                       'texture' )
+  eNode.addDoubleLink( 'Filter.gyrus',
+                       'gyrus' )
+  eNode.addDoubleLink( 'Filter.subject',
+                       'subject' )
+  eNode.addDoubleLink( 'Filter.subset_of_tract',
+                       'subset_of_tract' )
+  eNode.addDoubleLink( 'Filter.gyri_texture',
+                       'gyri_texture' )
 
-  ### 04 - Regroup Fibers From Length
-  eNode.addChild( 'TractsReorganisation',
-                  ProcessExecutionNode( 'regroupFibersFromLength',
-                  optional = 1 ) )
-
-  eNode.addDoubleLink( 'TractsReorganisation.gyrus_tracts_MeshClosestPoint',
-                       'PrepareTracts.gyrus_tracts_MeshClosestPoint' )
-
-  ### 05 - Select Fibers From Length
-  eNode.addChild( 'LengthFiltering',
-                  ProcessExecutionNode( 'selectFibersFromLength',
-                  optional = 1 ) )
-
-  eNode.addDoubleLink( 'LengthFiltering.tracts_by_length_MeshClosestPoint',
-                       'TractsReorganisation.tracts_by_length_MeshClosestPoint' )
-
-  ## Delete Intermediate Fibers
-  eNode.addChild( 'deleteTracts',
-                  ProcessExecutionNode( 'removeUnnecessaryBundles',
-                  optional = 1,
-                  selected = 0 ) )
-
-  eNode.addDoubleLink( 'deleteTracts.reorganized_subset_of_tract',
-                       'PrepareTracts.reorganized_subset_of_tract' )
-
-  eNode.addDoubleLink( 'deleteTracts.patch_label',
-                       'patch_label' )
-
-  ### 06 - Bundles Fusion Oversampled
+  ### Bundles Oversampled
   eNode.addChild( 'ConnectivityCortexMatrix',
                   ProcessExecutionNode( 'fiberOversampler',
                   optional = 1 ) )
 
   eNode.addDoubleLink( 'ConnectivityCortexMatrix.length_filtered_tracts_MeshIntersectPoint',
-                       'LengthFiltering.length_filtered_tracts_MeshIntersectPoint' )
+                       'Filter.subsets_of_tracts_distantFibers' )
 
-  ### 07 - Connectivity Matrix  Smooothed
+  ### Connectivity Matrix
   eNode.addChild( 'Smoothing',
                   ProcessExecutionNode( 'createConnectivityMatrix',
                   optional = 1 ) )
@@ -82,21 +62,21 @@ def initialization( self ):
                        'ConnectivityCortexMatrix.oversampled_tracts_MeshIntersectPoint' )
 
   eNode.addDoubleLink( 'Smoothing.length_filtered_tracts_MeshClosestPoint',
-                       'LengthFiltering.length_filtered_tracts_MeshClosestPoint' )
+                       'Filter.subsets_of_tracts_FibersNearCortex' )
 
-  #eNode.Smoothing.removeLink( 'gyri_segmentation',
+  #eNode.Smoothing.removeLink( 'gyri_texture',
                               #'oversampled_tracts_MeshIntersectPoint' )
 
   eNode.addDoubleLink( 'Smoothing.gyri_segmentation',
-                       'gyri_segmentation' )
+                       'gyri_texture' )
                        
   eNode.addDoubleLink( 'Smoothing.white_mesh',
                        'white_mesh' )
                        
   eNode.addDoubleLink( 'Smoothing.diff_to_anat_transform',
-                       'diff_to_anat_transform' )
+                       'dw_to_t1' )
 
-  ### 08 - Sum Sparse Matrix
+  ### Sum Sparse Matrix
   eNode.addChild( 'Sum',
                   ProcessExecutionNode( 'sumSparseMatrix',
                   optional = 1 ) )
@@ -108,15 +88,15 @@ def initialization( self ):
                        'white_mesh' )
 
   eNode.addDoubleLink( 'Sum.gyri_segmentation',
-                       'gyri_segmentation' )
+                       'gyri_texture' )
                        
   eNode.addDoubleLink( 'Sum.patch_label',
-                       'patch_label' )
+                       'gyrus' )
 
   eNode.addDoubleLink( 'Sum.smooth',
-                       'smooth' )
+                       'smoothing' )
 
-  ### 09 - Mean Connectivity Profile Texture intraSubjects
+  ### Mean Connectivity Profile
   eNode.addChild( 'ProfileComputing',
                   ProcessExecutionNode( 'createMeanConnectivityProfile',
                   optional = 1 ) )
@@ -125,9 +105,9 @@ def initialization( self ):
                        'Sum.connectivity_matrix_full' )
 
   eNode.addDoubleLink( 'ProfileComputing.gyri_segmentation',
-                       'gyri_segmentation' )
+                       'gyri_texture' )
 
-  ### 11 - Normed Smoothed Mean Connectivity Profile IntraSubjects
+  ### Normed Smoothed Mean Connectivity Profile
   eNode.addChild( 'ProfileNormalization',
                   ProcessExecutionNode( 'removeInternalConnections',
                   optional = 1 ) )
@@ -136,12 +116,12 @@ def initialization( self ):
                        'ProfileComputing.gyrus_mean_connectivity_profile' )
 
   eNode.addDoubleLink( 'ProfileNormalization.gyri_segmentation',
-                       'gyri_segmentation' )
+                       'gyri_texture' )
                        
   eNode.addDoubleLink( 'ProfileNormalization.white_mesh',
                        'white_mesh' )
 
-  ### 12 - Watershed On Normed Smoothed IntraSubjects
+  ### Watershed
   eNode.addChild( 'Watershed',
                   ProcessExecutionNode( 'watershedReflectingConnectionsToGyrus',
                   optional = 1 ) )
@@ -152,7 +132,7 @@ def initialization( self ):
   eNode.addDoubleLink( 'Watershed.white_mesh',
                        'white_mesh' )
 
-  ### 13 - Filtered Watershed On Normed Smoothed IntraSubjects
+  ### Filtered Watershed
   eNode.addChild( 'ConnectivityMatrixToBassins',
                   ProcessExecutionNode( 'filteringWatershed',
                   optional = 1 ) )
@@ -161,12 +141,12 @@ def initialization( self ):
                        'ProfileComputing.connectivity_matrix_full' )
 
   eNode.addDoubleLink( 'ConnectivityMatrixToBassins.gyri_segmentation',
-                       'gyri_segmentation')
+                       'gyri_texture')
 
   eNode.addDoubleLink( 'ConnectivityMatrixToBassins.white_mesh',
                        'white_mesh' )
 
-  # 14 - Connectivity Matrix to Watershed Basins IntraSubjects
+  # Reduced Connectivity Matrix
 
   eNode.addChild( 'ConnectivityMatrixWatershedToBassins',
                   ProcessExecutionNode( 'createReducedConnectivityMatrix',
@@ -176,12 +156,12 @@ def initialization( self ):
                        'ConnectivityMatrixToBassins.connectivity_matrix_full' )
 
   eNode.addDoubleLink( 'ConnectivityMatrixWatershedToBassins.gyri_segmentation',
-                       'gyri_segmentation' )
+                       'gyri_texture' )
                        
   eNode.addDoubleLink( 'ConnectivityMatrixWatershedToBassins.white_mesh',
                        'white_mesh' )
 
-  ### 15 - Clustering IntraSubjects
+  ### Clustering
   eNode.addChild( 'ClusteringIntraSubjects',
                   ProcessExecutionNode( 'ClusteringIntrasubject',
                   optional = 1 ) )
@@ -190,9 +170,43 @@ def initialization( self ):
                        'ConnectivityMatrixWatershedToBassins.connectivity_matrix_reduced' )
 
   eNode.addDoubleLink( 'ClusteringIntraSubjects.gyri_segmentation',
-                       'gyri_segmentation' )
+                       'gyri_texture' )
 
   eNode.addDoubleLink( 'ClusteringIntraSubjects.white_mesh',
                        'white_mesh' )
 
   self.setExecutionNode( eNode )
+
+
+
+#### 02/03 - Filtering per gyrus
+#eNode.addChild( 'PrepareTracts',
+                #ProcessExecutionNode( 'filteringPerGyrus',
+                #optional = 1 ) )
+#eNode.addDoubleLink( 'PrepareTracts.reorganized_subset_of_tract',
+                     #'reorganized_subset_of_tract' )
+#eNode.addDoubleLink( 'PrepareTracts.gyri_texture',
+                     #'gyri_texture' )
+#eNode.addDoubleLink( 'PrepareTracts.gyrus',
+                     #'gyrus' )
+#### 04 - Regroup Fibers From Length
+#eNode.addChild( 'TractsReorganisation',
+                #ProcessExecutionNode( 'regroupFibersFromLength',
+                #optional = 1 ) )
+#eNode.addDoubleLink( 'TractsReorganisation.gyrus_tracts_MeshClosestPoint',
+                     #'PrepareTracts.gyrus_tracts_MeshClosestPoint' )
+#### 05 - Select Fibers From Length
+#eNode.addChild( 'LengthFiltering',
+                #ProcessExecutionNode( 'selectFibersFromLength',
+                #optional = 1 ) )
+#eNode.addDoubleLink( 'LengthFiltering.tracts_by_length_MeshClosestPoint',
+                     #'TractsReorganisation.tracts_by_length_MeshClosestPoint' )
+### Delete Intermediate Fibers
+#eNode.addChild( 'deleteTracts',
+                #ProcessExecutionNode( 'removeUnnecessaryBundles',
+                #optional = 1,
+                #selected = 0 ) )
+#eNode.addDoubleLink( 'deleteTracts.reorganized_subset_of_tract',
+                     #'PrepareTracts.reorganized_subset_of_tract' )
+#eNode.addDoubleLink( 'deleteTracts.gyrus',
+                     #'gyrus' )
