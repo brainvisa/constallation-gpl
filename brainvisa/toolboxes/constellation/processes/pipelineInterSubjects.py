929132026eb9d97d9f95bff2ecbec0584ce3add4
lefranc <lefranc@bioproj.invalid>
2013-06-05 14:39:56 +0000

    clean code
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@56285 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/pipelineInterSubjects.py
+++ brainvisa/toolboxes/constellation/processes/pipelineInterSubjects.py
@@ -10,115 +10,68 @@ name = 'Freesurfer BrainVisa + Constellation Inter pipeline'
 userLevel = 2
 
 
-#signature = Signature(
-  #'list_of_subjects',ListOf ( ReadDiskItem("Subject", 'Directory') ),
-  ##'group_definition_FS', ReadDiskItem('Group definition', 'XML' ),
-  ##'group_definition_BV', ReadDiskItem('Group definition', 'XML' ),
-  #'gyri_segmentation', ReadDiskItem( 'BothResampledGyri', 'Texture' ),
-#)
+signature = Signature(
+  'list_of_subjects_FS', ListOf( ReadDiskItem( 'Subject', 'Directory' ) ),
+             'group_FS', ReadDiskItem('Group definition', 'XML' ),
+  'list_of_subjects_BV', ListOf( ReadDiskItem( 'Subject', 'Directory' ) ),
+             'group_BV', ReadDiskItem('Group definition', 'XML' ),
+                'study', String(),
+                'gyrus', Integer(),
+    'init_texture_name', String(),
+   'group_texture_name', String(),
+         'gyri_texture', ListOf( ReadDiskItem( 'BothResampledGyri', 'Aims texture formats' ) ),  
+)
 
-#def initialization( self ):
+def initialization( self ):
 
-  #eNode = SerialExecutionNode( self.name, parameterized=self )
+  eNode = SerialExecutionNode( self.name, parameterized=self )
 
-  #### Average Mesh FreeSurfer
-  ##eNode.addChild( 'average01',
-                  ##ProcessExecutionNode( 'freesurferCreateGroup',
-                  ##optional = 1 ) )
+  ### Average Mesh FreeSurfer
+  eNode.addChild( 'GroupFreesurfer',
+                  ProcessExecutionNode( 'freesurferCreateGroup',
+                  optional = 1 ) )
 
-  ##eNode.addDoubleLink( 'average01.list_of_subjects',
-                       ##'list_of_subjects' )
+  eNode.addDoubleLink( 'GroupFreesurfer.list_of_subjects',
+                       'list_of_subjects_FS' )
+  eNode.addDoubleLink( 'GroupFreesurfer.group_definition',
+                       'group_FS' )
 
-  ##eNode.addDoubleLink( 'average01.group_definition',
-                       ##'group_definition_FS' )
+  eNode.addChild( 'AverageBrain',
+                  ProcessExecutionNode( 'freesurferMeanMesh',
+                  optional = 1 ) )
 
-  ##eNode.addChild( 'average02',
-                  ##ProcessExecutionNode( 'freesurferMeanMesh',
-                  ##optional = 1 ) )
+  eNode.addDoubleLink( 'AverageBrain.group',
+                       'GroupFreesurfer.group_definition' )
 
-  ##eNode.addDoubleLink( 'average02.group',
-                       ##'average01.group_definition' )
+  ### Create of group BrainVisa
+  eNode.addChild( 'groupBrainvisa',
+                  ProcessExecutionNode( 'createGroup',
+                  optional = 1 ) )
 
-  #### Create of group BrainVisa
-  ##eNode.addChild( 'groupBV',
-                  ##ProcessExecutionNode( 'createGroup',
-                  ##optional = 1 ) )
+  eNode.addDoubleLink( 'groupBrainvisa.list_of_subjects',
+                       'list_of_subjects_BV' )
+  eNode.addDoubleLink( 'groupBrainvisa.group_definition',
+                       'group_BV' )
 
-  ##eNode.addDoubleLink( 'groupBV.list_of_subjects',
-                       ##'list_of_subjects' )
+  ### Brainvisa Constellation Inter Pipeline
+  eNode.addChild( 'ConstellationInter',
+                  ProcessExecutionNode( 'pipelineInterBrainvisa',
+                  optional = 1 ) )
 
-  ##eNode.addDoubleLink( 'groupBV.group_definition',
-                       ##'group_definition_BV' )
-  
-  ### 01 Surface With Enough Connections Creation InterSubjects
-  #eNode.addChild( 'meanProfileInter',
-                  #ProcessExecutionNode( 'surfaceWithEnoughConnectionsCreation',
-                  #optional = 1 ) )
-  
-  #eNode.addDoubleLink( 'meanProfileInter.group',
-                       #'groupBV.group_definition' )
-  
-  ### 02 Combine All Subjects Mean Conectivity Profile
-  #eNode.addChild( 'combineMeanInter',
-                  #ProcessExecutionNode( 'combineAllSubjectsMeanConnProfile',
-                  #optional = 1 ) )
-  
-  #eNode.addDoubleLink( 'combineMeanInter.group',
-                       #'meanProfileInter.group' )
-  
-  ### 03 Thresholding Average Mean Connectivity Profile
-  #eNode.addChild( 'meanInter',
-                  #ProcessExecutionNode( 'thresholdingAverageMeanConnectivityProfile',
-                  #optional = 1 ) )
-  
-  #eNode.addDoubleLink( 'meanInter.mean_connectivity_profile_texture_mask',
-                       #'meanProfileInter.mean_connectivity_profile_texture_mask' )
-                       
-  #eNode.addDoubleLink( 'meanInter.mean_connectivity_profile_all',
-                       #'combineMeanInter.mean_connectivity_profile_all' )
-  
-  ### 04 Watershed On Normed Smoothed InterSubjects
-  #eNode.addChild( 'watershedInter',
-                  #ProcessExecutionNode( 'watershedInterSubjects',
-                  #optional = 1 ) )
-                  
-  #eNode.addDoubleLink( 'watershedInter.average_mesh',
-                       #'average02.BothAverageMesh' )
-  
-  #eNode.addDoubleLink( 'watershedInter.normed_thresholded_mean_connectivity_profile',
-                       #'meanInter.norm_thresholded_mean_connectivity_profile' )
-  
-  ### 05 Connectivity Matrix to Watershed Bassins InterSubjects
-  #eNode.addChild( 'connMatrixBasinInter',
-                  #ProcessExecutionNode( 'ConnectivityMatrixToBassinsInterSubjects',
-                  #optional = 1 ) )
+  eNode.addDoubleLink( 'ConstellationInter.study_name',
+                       'study' )
+  eNode.addDoubleLink( 'ConstellationInter.patch_label',
+                       'gyrus' )
+  eNode.addDoubleLink( 'ConstellationInter.texture_in',
+                       'init_texture_name' )
+  eNode.addDoubleLink( 'ConstellationInter.texture_out',
+                       'group_texture_name' )
+  eNode.addDoubleLink( 'ConstellationInter.group',
+                       'group_BV' )
+  eNode.addDoubleLink( 'ConstellationInter.average_mesh',
+                       'AverageBrain.BothAverageMesh' )                  
+  eNode.addDoubleLink( 'ConstellationInter.connMatrixBasinInter.gyri_segmentation',
+                       'gyri_texture' )
 
-  #eNode.addDoubleLink( 'connMatrixBasinInter.group',
-                       #'combineMeanInter.group' )
-
-  #eNode.addDoubleLink( 'connMatrixBasinInter.average_mesh',
-                       #'watershedInter.average_mesh' )
-                       
-  #eNode.addDoubleLink( 'connMatrixBasinInter.gyri_segmentation',
-                       #'gyri_segmentation' )
-                       
-  #eNode.addDoubleLink( 'connMatrixBasinInter.filtered_watershed_basins',
-                       #'watershedInter.filtered_watershed_basins' )
-  
-  ### 06 Clustering InterSubjects
-  #eNode.addChild( 'clusteringInter',
-                  #ProcessExecutionNode( 'ClusteringInterSubjects',
-                  #optional = 1 ) )
-
-  #eNode.addDoubleLink( 'clusteringInter.group',
-                       #'connMatrixBasinInter.group' )
-
-
-  #eNode.addDoubleLink( 'clusteringInter.average_mesh',
-                       #'connMatrixBasinInter.average_mesh' )
-
-  #eNode.addDoubleLink( 'clusteringInter.gyri_segmentation',
-                       #'connMatrixBasinInter.gyri_segmentation' )
-  
-  #self.setExecutionNode( eNode )
+  self.setExecutionNode( eNode )
   
