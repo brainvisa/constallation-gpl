ddf0dc0cf16bc484bbd99bc1806fec6117bc559d
lefranc <lefranc@bioproj.invalid>
2013-06-05 14:39:24 +0000

    mainThread() and error in link function
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@56284 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/tools/inter/surfaceWithEnoughConnectionsCreation.py
+++ brainvisa/toolboxes/constellation/processes/tools/inter/surfaceWithEnoughConnectionsCreation.py
@@ -4,6 +4,7 @@ from brainvisa.group_utils import Subject
 from soma.minf.api import registerClass, readMinf
 from soma.path import find_in_path
 from soma import aims
+import threading
 
 def validation():
   if not find_in_path( 'constelConnectivityProfileOverlapMask.py' ):
@@ -24,7 +25,7 @@ signature = Signature(
 
 def initialization ( self ):
   def linkIndividualProfiles( self, dummy ):
-    if self.group is not None and self.individual_connectivity_profile is not None:
+    if self.group is not None:
       registerClass('minf_2.0', Subject, 'Subject')
       groupOfSubjects = readMinf(self.group.fullPath())
       profiles = []
@@ -32,8 +33,9 @@ def initialization ( self ):
         study = self.study_name
         texture = self.texture_in
         gyrus = 'G' + str(self.patch_label)
-        profiles.append( ReadDiskItem( 'Gyrus Connectivity Profile', 'Aims texture formats' ).findValue( { 'study': study, 'texture': texture, 'gyrus': gyrus }, subject.attributes() ) )
-        print profiles
+        profile = ReadDiskItem( 'Gyrus Connectivity Profile', 'Aims texture formats' ).findValue( { 'study': study, 'texture': texture, 'gyrus': gyrus }, subject.attributes() )
+        if profile is not None:
+          profiles.append( profile )
       return profiles
   def linkGroupProfiles( self, dummy ):
     if self.individual_connectivity_profile and self.group is not None:
@@ -47,7 +49,10 @@ def initialization ( self ):
   self.signature['individual_connectivity_profile'].userLevel = 2
 
 def execution ( self, context ):
-  mainThreadActions().call( aims.carto.PluginLoader.load )
+  if mainThread() is not threading.currentThread():
+    mainThreadActions().call( aims.carto.PluginLoader.load )
+  else:
+    aims.carto.PluginLoader.load()
 
   registerClass('minf_2.0', Subject, 'Subject')
   groupOfSubjects = readMinf(self.group.fullPath())
