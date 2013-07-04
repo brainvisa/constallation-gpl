2c99f88da6a371b6732f11f166fce811eb650ff1
lefranc <lefranc@bioproj.invalid>
2013-07-04 09:35:27 +0000

    new process: cleaning isolated vertices from gyri texture...
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@56880 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ brainvisa/toolboxes/constellation/processes/tools/gyriTextureCleaningIsolatedVertices.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from brainvisa.processes import *
+from soma.path import find_in_path
+
+name = 'Cleaning Isolated Vertices'
+userLevel = 2
+
+signature = Signature(
+  'gyri_texture', ReadDiskItem( 'BothResampledGyri', 'Aims texture formats' ),
+  'mesh', ReadDiskItem( 'AimsBothWhite', 'Aims mesh formats' ),
+  'gyri_texture_clean', WriteDiskItem( 'BothResampledGyri', 'Aims texture formats' ),
+)
+
+def initialization ( self ):
+  self.linkParameters( 'mesh', 'gyri_texture' )
+  self.linkParameters( 'gyri_texture_clean', 'gyri_texture' )
+  
+def execution ( self, context ):
+  context.system('python', find_in_path( 'constelGyriTextureCleaningIsolatedVertices.py' ),
+    '-i', self.gyri_texture,
+    '-m', self.mesh,
+    '-o', self.gyri_texture_clean
+  )
+  context.write( 'OK' )
