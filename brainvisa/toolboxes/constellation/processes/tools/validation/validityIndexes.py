495ee345e6826f51b69d74f4a39eec71a5b5ab93
lefranc <lefranc@bioproj.invalid>
2013-12-06 13:02:40 +0000

    new process (in progress)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@59866 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ brainvisa/toolboxes/constellation/processes/tools/validation/validityIndexes.py
@@ -0,0 +1,45 @@
+from brainvisa.processes import *
+from soma.path import find_in_path
+import constel.lib.clustervalidity as cv
+import constel.lib.plot as p
+import numpy as np
+import pylab
+
+name = 'Validity Indexes'
+userLevel = 2
+
+signature = Signature(
+  'matrix', ReadDiskItem( 'Group Matrix', 'GIS image' ),
+  'kmax', Integer(),
+  'nbIter', Integer(),
+  'indexFile', WriteDiskItem('Text File', ['Text File', 'CSV file'] ),
+)
+
+class MatplotlibFig( object ):
+  def __init__( self, fig ):
+    self._fig = fig
+  def __del__( self ):
+    mainThreadActions().call( pylab.close, self._fig )
+
+def initialization ( self ):
+  self.kmax = 12
+  self.nbIter = 100
+  self.indexFile = '/tmp/validityindexes.txt'
+
+def execution ( self, context ):
+  context.system( 'python', find_in_path( 'constelGettingValidityIndexes.py' ),
+    '-m', self.matrix,
+    '-n', self.nbIter,
+    '-k', self.kmax,
+    '-f', self.indexFile
+  )
+  context.write( 'OK' )
+  
+  fileR = open(self.indexFile, 'w')
+  
+  
+  
+  fileR.close()
+  
+  fig = mainThreadActions().call( p.validity_indexes_plot, sDB, sDunn, sCH, SFA, SFG, SFMed)
+  return MatplotlibFig( fig )
