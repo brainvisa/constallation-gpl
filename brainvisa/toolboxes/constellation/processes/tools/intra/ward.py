5f48acad3602e61490414b0026f6ad68dafdf4f2
lefranc <lefranc@bioproj.invalid>
2014-02-12 15:52:17 +0000

    to test
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@60913 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ brainvisa/toolboxes/constellation/processes/tools/intra/ward.py
@@ -0,0 +1,60 @@
+from brainvisa.processes import *
+from soma import aims
+import scipy
+import scipy.cluster.hierarchy
+import scipy.spatial.distance
+import os
+import numpy as np
+
+def validation():
+    if not find_in_path( 'constelConnectionDensityTexture' ):
+        raise ValidationError( 'constellation module is not here.' )
+
+name = 'Ward'
+userLevel = 2
+
+signature = Signature(
+    'reduced_connectivity_matrix', WriteDiskItem('Reduced Connectivity Matrix', 
+                                               'GIS image'),
+)
+
+def initialization (self):
+    pass
+
+def execution(self, context):
+    matrix = aims.read(self.reduced_connectivity_matrix)
+    mat = np.asarray(matrix)[:, :, 0, 0]
+    n, p = s = mat.shape
+    Z = scipy.cluster.hierarchy.linkage(mat, method='ward', metric='euclidean')
+    OUTPUT_DIR_FMT = '/home/sl236442'
+    TS = []
+    for nb in n_clusters:
+        print "Trying {nb} cluster(s)".format(nb=nb)
+        T = scipy.cluster.hierarchy.fcluster(Z,criterion='maxclust',t=nb)
+        T = T - 1
+        TS.append(T)
+        output_dir = OUTPUT_DIR_FMT.format(k=nb)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        assign_filename = os.path.join(output_dir, "assign.npy")
+        np.save(assign_filename, T)
+        centers = np.zeros((nb, p))
+        for i in range(nb):
+            index = np.where(T == i)[0]
+            all_data = mat[index]
+            mean_data = all_data.mean(axis=0)
+            centers[i] = mean_data
+        centers_filename = os.path.join(output_dir, "centers.npy")
+        np.save(centers_filename, centers)
+        dst_to_centers = scipy.spatial.distance.cdist(mat, centers)
+        dst_to_centers_filename = os.path.join(output_dir, "dst_to_centers.npy")
+        np.save(dst_to_centers_filename, dst_to_centers)
+        n_points_per_cluster_filename = os.path.join(output_dir, "n_points_per_cluster.txt")
+        f = open(n_points_per_cluster_filename, "w+")
+        for i in range(nb):
+            index = np.where(T == i)[0]
+            print >> f, index.shape[0]
+            dst_to_this_center = dst_to_centers[:, i]
+            closest_subject_index = np.argmin(dst_to_this_center)
+                                                                        
+        f.close()
