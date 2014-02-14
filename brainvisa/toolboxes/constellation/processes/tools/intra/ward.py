50cc4ee6761ced9ef96436882489f1ff5db6607b
lefranc <lefranc@bioproj.invalid>
2014-02-14 16:03:55 +0000

    add command: constelClusteringWard.py
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@60976 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/tools/intra/ward.py
+++ brainvisa/toolboxes/constellation/processes/tools/intra/ward.py
@@ -1,12 +1,7 @@
 from brainvisa.processes import *
-from soma import aims
-import scipy
-import scipy.cluster.hierarchy
-import scipy.spatial.distance
 from soma.path import find_in_path
 import matplotlib.pyplot as plt
-import os
-import numpy as np
+import scipy.cluster.hierarchy
 
 def validation():
     if not find_in_path( 'constelConnectionDensityTexture' ):
@@ -16,8 +11,17 @@ name = 'Clustering Ward'
 userLevel = 2
 
 signature = Signature( 
+    'kmax', Integer(),
+    'patch', Integer(),
     'group_matrix', ReadDiskItem('Group Matrix', 'GIS image'),
-    'output_directory', ReadDiskItem( 'Directory', 'Directory' ),
+    'average_mesh', ReadDiskItem('BothAverageBrainWhite', 
+                                 'BrainVISA mesh formats'),
+    'gyri_texture', ListOf(
+        ReadDiskItem('FreesurferResampledBothParcellationType', 
+                     'Aims texture formats')),
+    'tex_time', ListOf(WriteDiskItem('Group Clustering Time', 
+                                            'BrainVISA texture formats')),
+    'output_directory', ReadDiskItem('Directory', 'Directory'),
 )
 
 def initialization (self):
@@ -26,58 +30,12 @@ def initialization (self):
 def execution(self, context):
     '''Use hieararchical clustering to identify patterns.
     '''
-    # Load the matrix
-    matrix = aims.read(self.group_matrix.fullPath())
-    mat = np.asarray(matrix)[:, :, 0, 0]
-    n, p = s = mat.shape
-    # Compute linkage
-    Z = scipy.cluster.hierarchy.linkage(mat, method='ward', metric='euclidean')
-    OUTPUT_DIR = str(self.output_directory.fullPath())
-    OUTPUT_DIR_Z = OUTPUT_DIR + 'dendrogram.npy'
-    print 'OUTPUT_DIR_Z', OUTPUT_DIR_Z
-    np.save(OUTPUT_DIR_Z, Z)
-
-    ## Save dendrogram
-    #dendrogram_full_fig = plt.figure()
-    #R = scipy.cluster.hierarchy.dendrogram(Z,
-                                        #color_threshold=1,
-                                        #distance_sort='ascending')
-    #dendrogram_full_fig.savefig('/home/sl236442/dendrogram.full.svg')
-    #dendrogram_lastp_fig = plt.figure()
-    #R = scipy.cluster.hierarchy.dendrogram(Z,
-                                        #color_threshold=1,
-                                        #truncate_mode='lastp',
-                                        #distance_sort='ascending')
-    #dendrogram_lastp_fig.savefig('/home/sl236442/dendrogram.lastp.svg')
-    #OUTPUT_DIR_FMT = '/home/sl236442'
-    #TS = []
-    #for nb in n_clusters:
-        #print "Trying {nb} cluster(s)".format(nb=nb)
-        #T = scipy.cluster.hierarchy.fcluster(Z,criterion='maxclust',t=nb)
-        #T = T - 1
-        #TS.append(T)
-        #output_dir = OUTPUT_DIR_FMT.format(k=nb)
-        #if not os.path.exists(output_dir):
-            #os.makedirs(output_dir)
-        #assign_filename = os.path.join(output_dir, "assign.npy")
-        #np.save(assign_filename, T)
-        #centers = np.zeros((nb, p))
-        #for i in range(nb):
-            #index = np.where(T == i)[0]
-            #all_data = mat[index]
-            #mean_data = all_data.mean(axis=0)
-            #centers[i] = mean_data
-        #centers_filename = os.path.join(output_dir, "centers.npy")
-        #np.save(centers_filename, centers)
-        #dst_to_centers = scipy.spatial.distance.cdist(mat, centers)
-        #dst_to_centers_filename = os.path.join(output_dir, "dst_to_centers.npy")
-        #np.save(dst_to_centers_filename, dst_to_centers)
-        #n_points_per_cluster_filename = os.path.join(output_dir, "n_points_per_cluster.txt")
-        #f = open(n_points_per_cluster_filename, "w+")
-        #for i in range(nb):
-            #index = np.where(T == i)[0]
-            #print >> f, index.shape[0]
-            #dst_to_this_center = dst_to_centers[:, i]
-            #closest_subject_index = np.argmin(dst_to_this_center)
-                                                                        
-        #f.close()
+        
+    for x in self.gyri_texture:
+        args += [ '-g', x ]
+    for t in self.tex_time:
+        args += [ '-t', t ]
+    args += ['-k', self.kmax, '-l', self.patch, '-c', self.group_matrix, 
+        '-m', self.avg_mesh, '-o', self.output_directory]
+    context.system('python', find_in_path( 'constelClusteringWard.py' ),
+        *args)
