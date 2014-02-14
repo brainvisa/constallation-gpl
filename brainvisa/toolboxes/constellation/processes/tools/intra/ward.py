35da286d16219e3cba9ffa0611ea4712feb980cd
lefranc <lefranc@bioproj.invalid>
2014-02-14 09:04:18 +0000

    test on cluster
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@60959 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/tools/intra/ward.py
+++ brainvisa/toolboxes/constellation/processes/tools/intra/ward.py
@@ -32,49 +32,52 @@ def execution(self, context):
     n, p = s = mat.shape
     # Compute linkage
     Z = scipy.cluster.hierarchy.linkage(mat, method='ward', metric='euclidean')
-    np.save('/home/sl236442/dendrogram.npy', Z)
+    OUTPUT_DIR = str(self.output_directory.fullPath())
+    OUTPUT_DIR_Z = OUTPUT_DIR + 'dendrogram.npy'
+    print 'OUTPUT_DIR_Z', OUTPUT_DIR_Z
+    np.save(OUTPUT_DIR_Z, Z)
 
-    # Save dendrogram
-    dendrogram_full_fig = plt.figure()
-    R = scipy.cluster.hierarchy.dendrogram(Z,
-                                        color_threshold=1,
-                                        distance_sort='ascending')
-    dendrogram_full_fig.savefig('/home/sl236442/dendrogram.full.svg')
-    dendrogram_lastp_fig = plt.figure()
-    R = scipy.cluster.hierarchy.dendrogram(Z,
-                                        color_threshold=1,
-                                        truncate_mode='lastp',
-                                        distance_sort='ascending')
-    dendrogram_lastp_fig.savefig('/home/sl236442/dendrogram.lastp.svg')
-    OUTPUT_DIR_FMT = '/home/sl236442'
-    TS = []
-    for nb in n_clusters:
-        print "Trying {nb} cluster(s)".format(nb=nb)
-        T = scipy.cluster.hierarchy.fcluster(Z,criterion='maxclust',t=nb)
-        T = T - 1
-        TS.append(T)
-        output_dir = OUTPUT_DIR_FMT.format(k=nb)
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-        assign_filename = os.path.join(output_dir, "assign.npy")
-        np.save(assign_filename, T)
-        centers = np.zeros((nb, p))
-        for i in range(nb):
-            index = np.where(T == i)[0]
-            all_data = mat[index]
-            mean_data = all_data.mean(axis=0)
-            centers[i] = mean_data
-        centers_filename = os.path.join(output_dir, "centers.npy")
-        np.save(centers_filename, centers)
-        dst_to_centers = scipy.spatial.distance.cdist(mat, centers)
-        dst_to_centers_filename = os.path.join(output_dir, "dst_to_centers.npy")
-        np.save(dst_to_centers_filename, dst_to_centers)
-        n_points_per_cluster_filename = os.path.join(output_dir, "n_points_per_cluster.txt")
-        f = open(n_points_per_cluster_filename, "w+")
-        for i in range(nb):
-            index = np.where(T == i)[0]
-            print >> f, index.shape[0]
-            dst_to_this_center = dst_to_centers[:, i]
-            closest_subject_index = np.argmin(dst_to_this_center)
+    ## Save dendrogram
+    #dendrogram_full_fig = plt.figure()
+    #R = scipy.cluster.hierarchy.dendrogram(Z,
+                                        #color_threshold=1,
+                                        #distance_sort='ascending')
+    #dendrogram_full_fig.savefig('/home/sl236442/dendrogram.full.svg')
+    #dendrogram_lastp_fig = plt.figure()
+    #R = scipy.cluster.hierarchy.dendrogram(Z,
+                                        #color_threshold=1,
+                                        #truncate_mode='lastp',
+                                        #distance_sort='ascending')
+    #dendrogram_lastp_fig.savefig('/home/sl236442/dendrogram.lastp.svg')
+    #OUTPUT_DIR_FMT = '/home/sl236442'
+    #TS = []
+    #for nb in n_clusters:
+        #print "Trying {nb} cluster(s)".format(nb=nb)
+        #T = scipy.cluster.hierarchy.fcluster(Z,criterion='maxclust',t=nb)
+        #T = T - 1
+        #TS.append(T)
+        #output_dir = OUTPUT_DIR_FMT.format(k=nb)
+        #if not os.path.exists(output_dir):
+            #os.makedirs(output_dir)
+        #assign_filename = os.path.join(output_dir, "assign.npy")
+        #np.save(assign_filename, T)
+        #centers = np.zeros((nb, p))
+        #for i in range(nb):
+            #index = np.where(T == i)[0]
+            #all_data = mat[index]
+            #mean_data = all_data.mean(axis=0)
+            #centers[i] = mean_data
+        #centers_filename = os.path.join(output_dir, "centers.npy")
+        #np.save(centers_filename, centers)
+        #dst_to_centers = scipy.spatial.distance.cdist(mat, centers)
+        #dst_to_centers_filename = os.path.join(output_dir, "dst_to_centers.npy")
+        #np.save(dst_to_centers_filename, dst_to_centers)
+        #n_points_per_cluster_filename = os.path.join(output_dir, "n_points_per_cluster.txt")
+        #f = open(n_points_per_cluster_filename, "w+")
+        #for i in range(nb):
+            #index = np.where(T == i)[0]
+            #print >> f, index.shape[0]
+            #dst_to_this_center = dst_to_centers[:, i]
+            #closest_subject_index = np.argmin(dst_to_this_center)
                                                                         
-        f.close()
+        #f.close()
