b5704a2a341c70668a0b0fc1d009dcf8d960157c
lefranc <lefranc@bioproj.invalid>
2014-03-28 14:11:43 +0000

    added score
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@61634 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/tools/validation/measures.py
+++ brainvisa/toolboxes/constellation/processes/tools/validation/measures.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+############################################################################
 #  This software and supporting documentation are distributed by
 #      CEA/NeuroSpin, Batiment 145,
 #      91191 Gif-sur-Yvette cedex
@@ -29,12 +30,11 @@
 #
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL license version 2 and that you accept its terms.
+############################################################################
 
 from brainvisa.processes import *
 from soma import aims
 import numpy as np
-import pylab
-import itertools
 
 import constel.lib.misctools as clm
 import constel.lib.clustering.clusterstools as clcc
@@ -44,85 +44,95 @@ name = 'Scores'
 userLevel = 2
 
 signature = Signature(
-  'clustering_1', ReadDiskItem( 'Group Clustering Time', 'BrainVISA texture formats' ),
-  'clustering_2', ReadDiskItem( 'Group Clustering Time', 'BrainVISA texture formats' ),
-  'time_step', Integer(),
-  'directory', ReadDiskItem( 'Directory', 'Directory' )
+    'clustering_1', ReadDiskItem( 'Group Clustering Time', 
+                                  'BrainVISA texture formats' ),
+    'clustering_2', ReadDiskItem( 'Group Clustering Time', 
+                                  'BrainVISA texture formats' ),
+    'time_step', Integer(),
+    'directory', ReadDiskItem( 'Directory', 'Directory' )
 )
 
 def initialization ( self ):
   pass
 
 def execution ( self, context ):
-  clus1 = str( self.clustering_1.fullPath() )
-  clus2 = str( self.clustering_2.fullPath() )
-  
-  c1 = aims.read( clus1 )
-  c2 = aims.read( clus2 )
-  
-  labels1 = c1[ self.time_step ].arraydata()
-  labels2 = c2[ self.time_step ].arraydata()
-  
-  list1, list2 = clm.sameNbElements( labels1, labels2 )
-  
-  entropy1 = clcc.entropy( list1 )
-  entropy2 = clcc.entropy( list2 )
-
-  mi = cls.mutual_information( list1, list2 )
-  ri = cls.rand_index( list1, list2 )
-  
-  homogeneity = mi / ( entropy1 )
-  completeness = mi / ( entropy2 )
-  
-  if homogeneity + completeness == 0.0:
-    v_measure = 0.0
-  else:
-    v_measure = ( 2.0 * homogeneity * completeness
-                           / (homogeneity + completeness))
-  
-  group1 = os.path.basename( os.path.dirname( os.path.dirname( os.path.dirname( os.path.dirname( os.path.dirname( os.path.dirname( os.path.dirname( self.clustering_1.fullPath() ) ) ) ) ) ) ) )
-  group2 = os.path.basename( os.path.dirname( os.path.dirname( os.path.dirname( os.path.dirname( os.path.dirname( os.path.dirname( os.path.dirname( self.clustering_2.fullPath() ) ) ) ) ) ) ) )
-  gyrus = os.path.basename( os.path.dirname( os.path.dirname( os.path.dirname( self.clustering_1.fullPath() ) ) ) )
-  scoreFile = str(self.directory.fullPath() ) + '/' + str(group1) + '_' + str(group2) + '_step' + str(self.time_step) + '_'+ str(gyrus) + '_' + 'scoreFile.txt'
-  
-  fileR = open( scoreFile, 'w')
-  fileR.write( 'Clustering 1 -----> ' + clus1 + '\n')
-  fileR.write( '\n' )
-  fileR.write( 'Clustering 2 -----> ' + clus2 + '\n')
-  fileR.write( '\n' )
-  lineEntropy1 = ''
-  lineEntropy2 = ''
-  lineRI = ''
-  lineMI = ''
-  lineHomogeneity = ''
-  lineCompleteness = ''
-  lineVmeasure = ''
-  
-  lineEntropy1 = lineEntropy1 + str(entropy1) + ' '
-  lineEntropy2 = lineEntropy2 + str(entropy2) + ' '
-  lineRI = lineRI + str(ri) + ' '
-  lineMI = lineMI + str(mi) + ' '
-  lineHomogeneity = lineHomogeneity + str(homogeneity) + ' '
-  lineCompleteness = lineCompleteness + str(completeness) + ' '
-  lineVmeasure = lineVmeasure + str(v_measure) + ' '
-  
-  fileR.write( '-----> entropy (clustering 1)   : ' + lineEntropy1 + '\n' )
-  fileR.write( '\n' )
-  fileR.write( '-----> entropy (clustering 2)   : ' + lineEntropy2 + '\n' )
-  fileR.write( '\n' )
-  fileR.write( '-----> rand index               : ' + lineRI + '\n' )
-  fileR.write( '\n' )
-  fileR.write( '-----> mutual information       : ' + lineMI + '\n' )
-  fileR.write( '\n' )
-  fileR.write( '-----> homogeneity              : ' + lineHomogeneity + '\n' )
-  fileR.write( '\n' )
-  fileR.write( '-----> completeness             : ' + lineCompleteness + '\n' )
-  fileR.write( '\n' )
-  fileR.write( '-----> v_measure                : ' + lineVmeasure + '\n' )
-
-  fileR.close()
+    clus1 = str(self.clustering_1.fullPath())
+    clus2 = str(self.clustering_2.fullPath())
     
+    c1 = aims.read(clus1)
+    c2 = aims.read(clus2)
+    
+    labels1 = c1[self.time_step].arraydata()
+    labels2 = c2[self.time_step].arraydata()
+    
+    list1, list2 = clm.sameNbElements(labels1, labels2)
+    
+    entropy1 = clcc.entropy(list1)
+    entropy2 = clcc.entropy(list2)
+
+    mi = cls.mutual_information(list1, list2)
+    ri = cls.rand_index(list1, list2)
+    
+    homogeneity = mi / (entropy1)
+    completeness = mi / (entropy2)
   
+    if homogeneity + completeness == 0.0:
+        v_measure = 0.0
+    else:
+        v_measure = (2.0 * homogeneity * completeness
+                            / (homogeneity + completeness))
   
+    cramer_v = cls.cramer_v(list1, list2)
   
-  
+    group1 = os.path.basename(os.path.dirname(os.path.dirname(
+             os.path.dirname(os.path.dirname(os.path.dirname(
+             os.path.dirname(os.path.dirname(self.clustering_1.fullPath()))
+             ))))))
+    group2 = os.path.basename(os.path.dirname(os.path.dirname(
+             os.path.dirname(os.path.dirname(os.path.dirname(
+             os.path.dirname(os.path.dirname(self.clustering_2.fullPath()))
+             ))))))
+    gyrus = os.path.basename(os.path.dirname(os.path.dirname(
+            os.path.dirname(self.clustering_1.fullPath()))))
+    scoreFile = str(self.directory.fullPath()) + '/' + str(group1) + '_' + str(group2) + '_step' + str(self.time_step) + '_'+ str(gyrus) + '_' + 'scoreFile.txt'
+    
+    fileR = open(scoreFile, 'w')
+    fileR.write('Clustering 1 -----> ' + clus1 + '\n')
+    fileR.write('\n' )
+    fileR.write('Clustering 2 -----> ' + clus2 + '\n')
+    fileR.write('\n' )
+    lineEntropy1 = ''
+    lineEntropy2 = ''
+    lineRI = ''
+    lineMI = ''
+    lineHomogeneity = ''
+    lineCompleteness = ''
+    lineVmeasure = ''
+    lineCramerV = ''
+    
+    lineEntropy1 = lineEntropy1 + str(entropy1) + ' '
+    lineEntropy2 = lineEntropy2 + str(entropy2) + ' '
+    lineRI = lineRI + str(ri) + ' '
+    lineMI = lineMI + str(mi) + ' '
+    lineHomogeneity = lineHomogeneity + str(homogeneity) + ' '
+    lineCompleteness = lineCompleteness + str(completeness) + ' '
+    lineVmeasure = lineVmeasure + str(v_measure) + ' '
+    lineCramerV = lineCramerV + str(cramer_v) + ' '
+    
+    fileR.write('-----> entropy (clustering 1)   : ' + lineEntropy1 + '\n')
+    fileR.write('\n')
+    fileR.write('-----> entropy (clustering 2)   : ' + lineEntropy2 + '\n')
+    fileR.write('\n')
+    fileR.write('-----> rand index               : ' + lineRI + '\n')
+    fileR.write('\n')
+    fileR.write('-----> mutual information       : ' + lineMI + '\n')
+    fileR.write('\n')
+    fileR.write('-----> homogeneity              : ' + lineHomogeneity + '\n')
+    fileR.write('\n')
+    fileR.write('-----> completeness             : ' + lineCompleteness + '\n')
+    fileR.write('\n')
+    fileR.write('-----> v_measure                : ' + lineVmeasure + '\n')
+    fileR.write('\n')
+    fileR.write('-----> cramer\'s V              : ' + lineCramerV + '\n')
+
+    fileR.close()
