6057be30560fcd851b40a9d1aece4e7ca4cc97ff
lefranc <lefranc@bioproj.invalid>
2014-04-04 15:01:30 +0000

    constel-gpl/process: several scores
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@61746 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/tools/validation/measures.py
+++ brainvisa/toolboxes/constellation/processes/tools/validation/measures.py
@@ -32,55 +32,40 @@
 # knowledge of the CeCILL license version 2 and that you accept its terms.
 ############################################################################
 
+# BrainVisa
 from brainvisa.processes import *
 from soma import aims
-import numpy as np
-import matplotlib.pyplot as plt
-import os
 
-import constel.lib.misctools as clm
-import constel.lib.clustering.clusterstools as clcc
-import constel.lib.measuringtools as cls
+# System module
+import numpy as np
+
+# Constellation
+from constel.lib.misctools import sameNbElements
+from constel.lib.clustering.clusterstools import entropy
+import constel.lib.measuringtools as measure
 
 name = 'Scores'
 userLevel = 2
 
 signature = Signature(
-    'clustering_1', ReadDiskItem( 'Group Clustering Time', 
-                                  'BrainVISA texture formats' ),
-    'clustering_2', ReadDiskItem( 'Group Clustering Time', 
-                                  'BrainVISA texture formats' ),
-    'time_step_min', Integer(),                              
+    'clustering_1', ReadDiskItem('Group Clustering Time', 
+                                 'BrainVISA texture formats'),
+    'clustering_2', ReadDiskItem('Group Clustering Time', 
+                                 'BrainVISA texture formats'),                            
     'time_step_max', Integer(),
-    'output_dir', ReadDiskItem( 'Directory', 'Directory' )
+    'output_dir', ReadDiskItem('Directory', 'Directory')
 )
 
 def mkdir_path(path):
     if not os.access(path, os.F_OK):
         os.makedirs(path)
            
-def initialization ( self ):
+def initialization (self):
     pass
-    #self.name_partition = "partition_"
+    # TO DO: link parameters between clustering_1 and 2. Group?
     
-    #def find_complementary_group(self, dummy):
-        #if self.clustering_1 is not None:
-            #attrs = dict( self.clustering_1.hierarchyAttributes() )
-            #attrs['group_of_subjects'] = self.complementary_group
-            #attrs['subject'] =  self.clustering_1.get('subject')
-            #attrs['study'] = self.clustering_1.get('study')
-            #attrs['texture'] = self.clustering_1.get('texture')
-            #attrs['gyrus'] = self.clustering_1.get('gyrus')
-            #attrs['smoothing'] = self.clustering_1.get('smoothing')
-            #filename = self.signature['clustering_2'].findValue( attrs )
-            #print attrs
-            #print 'filename', filename
-        #return filename
-        
-    #self.linkParameters('clustering_2', 'clustering_1', find_complementary_group)
-    #self.signature['clustering_2'].userLevel = 3
-
 def execution ( self, context ): 
+    # extract name of path
     group1 = os.path.basename(os.path.dirname(os.path.dirname(
              os.path.dirname(os.path.dirname(os.path.dirname(
              os.path.dirname(os.path.dirname(self.clustering_1.fullPath()))
@@ -91,109 +76,64 @@ def execution ( self, context ):
              ))))))
     gyrus = os.path.basename(os.path.dirname(os.path.dirname(
             os.path.dirname(self.clustering_1.fullPath()))))
-            
+    
+    # directory for all validation results organized by gyrus
     output_dir = os.path.join(self.output_dir.fullPath(), "validation", gyrus)
-    print output_dir
     mkdir_path(output_dir)
-    output_ri = os.path.join(output_dir, group1 + "_" + group2 + "_" + gyrus + "_" + "rand_index.npy")
-    #output_fig = os.path.join(output_dir, "rand_index.svg")
     
-    clus1 = str(self.clustering_1.fullPath())
-    clus2 = str(self.clustering_2.fullPath())
+    # directory for all measures
+    output = os.path.join(output_dir, group1 + "_" + group2 + "_" + gyrus + "_")
     
-    c1 = aims.read(clus1)
-    c2 = aims.read(clus2)
+    # read clustering texture (aims)
+    c1 = aims.read(self.clustering_1.fullPath())
+    c2 = aims.read(self.clustering_2.fullPath())
     
-    rand_index = []
-    for i in range(self.time_step_min, self.time_step_max + 1):
+    # calculate measure between two clustering (1 and 2)
+    randindex_values = []
+    cramer_values = []
+    mutualinformation_values = []
+    homogeneity = []
+    completeness = []
+    v_measure = []
+    for i in range(2, self.time_step_max + 1):
+        # list of labels (1 and 2)
         labels1 = c1[i].arraydata()
         labels2 = c2[i].arraydata()
-    
-        list1, list2 = clm.sameNbElements(labels1, labels2, False)
         
-        ri = cls.rand_index(list1, list2)
-        rand_index.append(ri)
-    np.savetxt(output_ri, rand_index)
-    
-    
-    
-    #rand_index_fig = plt.figure()
-    #plt.plot(range(self.time_step_min, self.time_step_max + 1), rand_index, 'b', label=self.name_partition)
-    #plt.plot(range(self.time_step_min, self.time_step_max + 1), rand_index, 'ro')
-    #plt.ylabel('Rand Index')
-    #plt.xlabel('Clusters (K)')
-    #rand_index_fig.suptitle(self.name_patch)
-    #plt.legend()
-    #rand_index_fig.savefig(output_fig)
-    
-    
-    #entropy1 = clcc.entropy(list1)
-    #entropy2 = clcc.entropy(list2)
+        # keep the same number of elements
+        l1, l2 = sameNbElements(labels1, labels2)
+        
+        # measure value for K
+        ri = measure.rand_index(l1, l2)
+        cv = measure.cramer_v(l1, l2)
+        mi = measure.mutual_information(l1, l2)
+        
+        # calculate entropy for each clustering
+        entropy1 = entropy(l1)
+        entropy2 = entropy(l2)
+        
+        # calculate homogeneity and completeness
+        homog = mi / (entropy1)
+        compl = mi / (entropy2)
+        
+        # calculate V measure: equivalent to the mutual information
+        if homog + compl == 0.0:
+            v = 0.0
+        else:
+            v = (2.0 * homog * compl / (homog + compl))
 
-    #mi = cls.mutual_information(list1, list2)
-    
-    
-    
-    #homogeneity = mi / (entropy1)
-    #completeness = mi / (entropy2)
-  
-    #if homogeneity + completeness == 0.0:
-        #v_measure = 0.0
-    #else:
-        #v_measure = (2.0 * homogeneity * completeness
-                            #/ (homogeneity + completeness))
-  
-    #cramer_v = cls.cramer_v(labels1, labels2)
-  
-    #group1 = os.path.basename(os.path.dirname(os.path.dirname(
-             #os.path.dirname(os.path.dirname(os.path.dirname(
-             #os.path.dirname(os.path.dirname(self.clustering_1.fullPath()))
-             #))))))
-    #group2 = os.path.basename(os.path.dirname(os.path.dirname(
-             #os.path.dirname(os.path.dirname(os.path.dirname(
-             #os.path.dirname(os.path.dirname(self.clustering_2.fullPath()))
-             #))))))
-    #gyrus = os.path.basename(os.path.dirname(os.path.dirname(
-            #os.path.dirname(self.clustering_1.fullPath()))))
-    #scoreFile = str(self.directory.fullPath()) + '/' + str(group1) + '_' + str(group2) + '_step' + str(self.time_step) + '_'+ str(gyrus) + '_' + 'scoreFile.txt'
-    
-    #fileR = open(scoreFile, 'w')
-    #fileR.write('Clustering 1 -----> ' + clus1 + '\n')
-    #fileR.write('\n' )
-    #fileR.write('Clustering 2 -----> ' + clus2 + '\n')
-    #fileR.write('\n' )
-    #lineEntropy1 = ''
-    #lineEntropy2 = ''
-    #lineRI = ''
-    #lineMI = ''
-    #lineHomogeneity = ''
-    #lineCompleteness = ''
-    #lineVmeasure = ''
-    #lineCramerV = ''
-    
-    #lineEntropy1 = lineEntropy1 + str(entropy1) + ' '
-    #lineEntropy2 = lineEntropy2 + str(entropy2) + ' '
-    #lineRI = lineRI + str(ri) + ' '
-    #lineMI = lineMI + str(mi) + ' '
-    #lineHomogeneity = lineHomogeneity + str(homogeneity) + ' '
-    #lineCompleteness = lineCompleteness + str(completeness) + ' '
-    #lineVmeasure = lineVmeasure + str(v_measure) + ' '
-    #lineCramerV = lineCramerV + str(cramer_v) + ' '
-    
-    #fileR.write('-----> entropy (clustering 1)   : ' + lineEntropy1 + '\n')
-    #fileR.write('\n')
-    #fileR.write('-----> entropy (clustering 2)   : ' + lineEntropy2 + '\n')
-    #fileR.write('\n')
-    #fileR.write('-----> rand index               : ' + lineRI + '\n')
-    #fileR.write('\n')
-    #fileR.write('-----> mutual information       : ' + lineMI + '\n')
-    #fileR.write('\n')
-    #fileR.write('-----> homogeneity              : ' + lineHomogeneity + '\n')
-    #fileR.write('\n')
-    #fileR.write('-----> completeness             : ' + lineCompleteness + '\n')
-    #fileR.write('\n')
-    #fileR.write('-----> v_measure                : ' + lineVmeasure + '\n')
-    #fileR.write('\n')
-    #fileR.write('-----> cramer\'s V               : ' + lineCramerV + '\n')
+        # list of measure values for K = 2:kmax
+        randindex_values.append(ri)
+        cramer_values.append(cv)
+        mutualinformation_values.append(mi)
+        homogeneity.append(homog)
+        completeness.append(compl)
+        v_measure.append(v)
 
-    #fileR.close()
+    # save in a npy file
+    np.savetxt(output + "rand_index.npy", randindex_values)
+    np.savetxt(output + "cramer_V.npy", cramer_values)
+    np.savetxt(output + "mutual_information.npy", mutualinformation_values)
+    np.savetxt(output + "homogeneity.npy", homogeneity)
+    np.savetxt(output + "completeness.npy", completeness)
+    np.savetxt(output + "v_measure.npy", v_measure)
