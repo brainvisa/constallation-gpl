bbc9bbf31b0c4bb73487c33f8283e26407b8bb7a
lefranc <lefranc@bioproj.invalid>
2014-06-23 13:55:55 +0000

    changes of names, pep8
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@62597 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/tools/convertSparseMatrixToIma.py
+++ brainvisa/toolboxes/constellation/processes/tools/convertSparseMatrixToIma.py
@@ -3,23 +3,23 @@ from brainvisa.processes import *
 from soma.path import find_in_path
 
 def validation():
-  if not find_in_path( 'AimsSparseMatrixToDense.py' ):
-    raise ValidationError( 'aims module is not here.' )
+    if not find_in_path('AimsSparseMatrixToDense.py'):
+        raise ValidationError('aims module is not here.')
 
-name = 'Convert Sparse Matrix to Ima'
+name = 'Convert Sparse Matrix to Dense'
 userLevel = 2
 
 signature = Signature(
-  'connectivity_matrix_full', ReadDiskItem( 'Gyrus Connectivity Matrix', 'Matrix sparse' ),
-  'patch_connectivity_matrix', WriteDiskItem( 'Patch Connectivity Matrix', 'GIS image' ),
-)
+    'sparse_connectivity_matrix', ReadDiskItem('Gyrus Connectivity Matrix',
+                                               'Matrix sparse'),
+    'dense_connectivity_matrix', WriteDiskItem('Patch Connectivity Matrix',
+                                               'GIS image'))
 
-def initialization ( self ):
-  self.linkParameters( 'patch_connectivity_matrix', 'connectivity_matrix_full' )
+def initialization(self):
+    self.linkParameters('dense_connectivity_matrix', 
+                        'sparse_connectivity_matrix')
 
-def execution ( self, context ):
-  context.system( 'AimsSparseMatrixToDense.py',
-    '-i', self.connectivity_matrix_full,
-    '-o', self.patch_connectivity_matrix,
-  )
-  context.write( 'Convert sparse matrix to ima : OK' )
+def execution(self, context):
+    context.system('AimsSparseMatrixToDense.py',
+                   '-i', self.sparse_connectivity_matrix,
+                   '-o', self.dense_connectivity_matrix)
