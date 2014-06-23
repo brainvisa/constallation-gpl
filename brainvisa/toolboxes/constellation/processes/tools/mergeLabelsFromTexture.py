026fc0d376c0e928f3b83abfd3e0498f29cef76c
lefranc <lefranc@bioproj.invalid>
2014-06-23 14:16:41 +0000

    change of names, pep8
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@62600 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/tools/mergeLabelsFromTexture.py
+++ brainvisa/toolboxes/constellation/processes/tools/mergeLabelsFromTexture.py
@@ -3,31 +3,37 @@ from brainvisa.processes import *
 from soma.path import find_in_path
 
 def validation():
-  if not find_in_path( 'AimsMergeLabelsFromTexture.py' ):
-    raise ValidationError( 'aims module is not here.' )
-  if not find_in_path( 'AimsExtractLabelsFromTexture.py'):
-    raise ValidationError( 'aims module is not here.' )
+    if not find_in_path('AimsMergeLabelsFromTexture.py'):
+        raise ValidationError('aims module is not here.')
+    if not find_in_path('AimsExtractLabelsFromTexture.py'):
+        raise ValidationError('aims module is not here.')
 
 name = 'Merge Labels From texture'
 userLevel = 2
 
 signature = Signature(
-  'gyri_texture', ReadDiskItem( 'FreesurferResampledBothParcellationType', 'Aims texture formats' ),
-  'old_label', ListOf( Integer() ),
-  'new_label', Integer(),
-  'gyri_output_texture', WriteDiskItem( 'FreesurferResampledBothParcellationType', 'Aims texture formats' ),
-  'keep_only_merged_regions', Boolean(),
-)
+    'gyri_texture', ReadDiskItem('FreesurferResampledBothParcellationType',
+                                 'Aims texture formats'),
+    'old_labels', ListOf(Integer()),
+    'new_label', Integer(),
+    'new_gyri_texture', WriteDiskItem('FreesurferResampledBothParcellationType',
+                                      'Aims texture formats'),
+    'keep_only_merged_regions', Boolean(),)
 
-def initialization ( self ):
-  self.keep_only_merged_regions = False
+def initialization(self):
+    self.keep_only_merged_regions = False
 
-def execution ( self, context ):
-  cmd_args = []
-  for ol in self.old_label:
-    cmd_args += [ '-l', ol ]
-  cmd_args += [ '-i', self.gyri_texture, '-n', self.new_label, '-o', self.gyri_output_texture ]
-  if  not self.keep_only_merged_regions:
-    context.system( 'python', find_in_path( 'AimsMergeLabelsFromTexture.py' ), *cmd_args )
-  else:
-    context.system( 'python', find_in_path( 'AimsExtractLabelsFromTexture.py' ), *cmd_args )
+def execution(self, context):
+    cmd_args = []
+    for n in self.old_labels:
+        cmd_args += ['-l', n]
+    cmd_args += ['-i', self.gyri_texture, '-n', self.new_label, 
+                 '-o', self.new_gyri_texture]
+    if not self.keep_only_merged_regions:
+        context.system('python', 
+                       find_in_path('AimsMergeLabelsFromTexture.py'), 
+                       *cmd_args)
+    else:
+        context.system('python', 
+                       find_in_path('AimsExtractLabelsFromTexture.py'), 
+                       *cmd_args)
