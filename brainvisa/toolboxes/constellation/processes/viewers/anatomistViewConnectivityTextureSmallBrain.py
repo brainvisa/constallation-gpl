65addfbccff88a128c080695f3f175b9273c929e
denisri <denis.riviere.gm@gmail.com>
2014-07-09 12:31:47 +0000

    parameters to specify distance / scaling in small brains
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@62925 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/viewers/anatomistViewConnectivityTextureSmallBrain.py
+++ brainvisa/toolboxes/constellation/processes/viewers/anatomistViewConnectivityTextureSmallBrain.py
@@ -35,39 +35,44 @@ from soma import aims
 import numpy
 import constel
 try:
-  import constel.lib.connmatrix.connmatrixtools as Mat
+    import constel.lib.connmatrix.connmatrixtools as Mat
 except:
-  pass
+    pass
 
 def validation():
-  try:
-    import constel.lib.connmatrix.connmatrixtools as Mat
-  except:
-    raise ValidationError( 'constel module is not available' )
+    try:
+        import constel.lib.connmatrix.connmatrixtools as Mat
+    except:
+        raise ValidationError( 'constel module is not available' )
 
 name = 'Anatomist view connectivity texture on small brains'
 userLevel = 2
 roles = ( 'viewer', )
 
 signature = Signature(
-  'connectivity_matrix', ListOf(ReadDiskItem('Group Matrix', 'aims readable volume formats')),
-  'mesh', ReadDiskItem( 'BothAverageBrainWhite', 'Aims mesh formats' ),
-  'basins_texture', ListOf(ReadDiskItem( 'Avg Filtered Watershed', 'anatomist texture formats')),
-  'clustering_texture', ListOf(ReadDiskItem('Group Clustering Texture', 'anatomist texture formats')),
-  'time_step', ListOf(Integer()),
-  'texture_hbm', ReadDiskItem('Label Texture', 'anatomist texture formats'),
+    'connectivity_matrix', ListOf(ReadDiskItem('Group Matrix', 'aims readable volume formats')),
+    'mesh', ReadDiskItem( 'BothAverageBrainWhite', 'Aims mesh formats' ),
+    'basins_texture', ListOf(ReadDiskItem( 'Avg Filtered Watershed', 'anatomist texture formats')),
+    'clustering_texture', ListOf(ReadDiskItem('Group Clustering Texture', 'anatomist texture formats')),
+    'time_step', ListOf(Integer()),
+    'texture_hbm', ReadDiskItem('Label Texture', 'anatomist texture formats'),
+    'small_brains_distance', Float(),
+    'small_brains_scaling', Float(),
 )
 
 def initialization( self ):
-  self.linkParameters( 'mesh', 'connectivity_matrix' )
-  self.linkParameters( 'basins_texture', 'connectivity_matrix' )
-  self.linkParameters( 'clustering_texture', 'connectivity_matrix' )
+    self.linkParameters( 'mesh', 'connectivity_matrix' )
+    self.linkParameters( 'basins_texture', 'connectivity_matrix' )
+    self.linkParameters( 'clustering_texture', 'connectivity_matrix' )
+    self.small_brains_distance = 10.
+    self.small_brains_scaling = 0.1
 
 def execution_mainthread( self, context ):
   a = ana.Anatomist()
   mesh = a.loadObject( self.mesh )
-  toto = []
+  graph_list = []
   for ct, matrix_name in enumerate(self.connectivity_matrix):
+      gyrus_name = os.path.basename(matrix_name.fullName())
       context.write(matrix_name)
       context.write('clustering_texture:', self.clustering_texture[ct])
       anaclusters = a.loadObject( self.clustering_texture[ct] )
@@ -85,19 +90,24 @@ def execution_mainthread( self, context ):
       # Create AimsTimeSurface_3
       aimsmesh = a.toAimsObject( mesh )
       avertex = numpy.asarray( aimsmesh.vertex() )
-      reducedMatrix = Mat.connMatrixParcelsToTargets( numpy.transpose(matrix), clusters, self.time_step[ct] )
+      # reduce matrix for each cluster (to watershed basins)
+      reducedMatrix = Mat.connMatrixParcelsToTargets( numpy.transpose(matrix), 
+          clusters, self.time_step[ct] )
       mat = numpy.asarray( reducedMatrix )
       mat = mat.reshape( mat.shape[:2] )
-  
+
       graph = aims.Graph( 'RoiArg' )
       bbmin, bbmax = mesh.boundingbox()
-      graph[ 'boundingbox_min' ] = list( bbmin )
-      graph[ 'boundingbox_max' ] = list( bbmax )
+      graph['boundingbox_min'] = list( bbmin )
+      graph['boundingbox_max'] = list( bbmax )
+      graph['small_brains_distance'] = self.small_brains_distance
+      graph['small_brains_scaling'] = self.small_brains_scaling
       # Complete list of clusters
       regions = [ x for x in numpy.unique( aclusters ) if x != 0 ]
       for rnum in regions:
           vertex = graph.addVertex( 'roi' )
           aims.GraphManip.storeAims( graph, vertex, 'aims_Tmtktri', aimsmesh )
+          #vertex['label'] = '%s - clust. %d' % (gyrus_name, rnum)
 
       anagraph = a.toAObject( graph )
       anagraph.releaseAppRef()
@@ -106,15 +116,19 @@ def execution_mainthread( self, context ):
       for i, vertex in enumerate( graph.vertices() ):
           anavertex = vertex[ 'ana_object' ]
           context.write('mat:', mat.shape, ', i:', i)
-          current_tex = constel.oneTargetDensityTargetsRegroupTexture( mat[i], basins, self.time_step[ct] )
+          # make texture for a cluster to basins
+          current_tex = constel.oneTargetDensityTargetsRegroupTexture( mat[i], 
+              basins, self.time_step[ct] )
           anatex = a.toAObject( current_tex )
           anatex.setPalette('Blue-Red-fusion')
           a.execute('TexturingParams', objects=[anatex], interpolation='rgb')
+          # TODO: set min/max to be the same on all vertices textures
           anatex.releaseAppRef()
           a.unmapObject( anatex )
           mesh2 = mesh.clone( True ) # shallow copy
           mesh2.setReferential( mesh.referential )
-          texsurf = a.fusionObjects( [ mesh2, anatex ], method='FusionTexSurfMethod' )
+          texsurf = a.fusionObjects( [ mesh2, anatex ], 
+              method='FusionTexSurfMethod' )
           a.unmapObject( texsurf )
           oldmesh = [ x for x in anavertex ][0]
           anavertex.setReferentialInheritance( mesh2 )
@@ -122,6 +136,8 @@ def execution_mainthread( self, context ):
           anavertex.insert( texsurf )
           clustindices = numpy.where( aclusters == regions[i] )[0]
           vertices = avertex[ clustindices ]
+          # TODO: could replace the barycenter by 
+          # roca.lib.meshTools.geodesicGravitycenter
           cent = numpy.average( vertices, axis=0 )
           vertex[ 'center' ] = cent
           vertex[ 'cluster_index' ] = i
@@ -129,26 +145,26 @@ def execution_mainthread( self, context ):
       anagraph.notifyObservers()
       a.mapObject( anagraph )
       anagraph.setMaterial( diffuse=[ 0.8, 0.8, 0.8, 1. ] )
-      toto.append(anagraph)
-      
+      graph_list.append(anagraph)
+
   anacl = a.loadObject( self.texture_hbm )
   anacl.setPalette( palette='parcellation720', minVal=11, maxVal=730,
     absoluteMode=True )
-  tex = a.fusionObjects( [ mesh, anacl  ], method='FusionTexSurfMethod' )
-  a.execute('TexturingParams', objects=[tex], interpolation='rgb')
+  big_textured_mesh = a.fusionObjects( [ mesh, anacl  ], method='FusionTexSurfMethod' )
+  a.execute('TexturingParams', objects=[big_textured_mesh], interpolation='rgb')
   wgroup = a.createWindowsBlock( nbCols=2 )
   win = a.createWindow( '3D', block=wgroup )
   win2 = a.createWindow( '3D', block=wgroup )
   #br = a.createWindow( 'Browser', block=wgroup )
-  win.addObjects(tex)
-  win.addObjects(toto)
+  win.addObjects(big_textured_mesh)
+  win.addObjects(graph_list)
   #br.addObjects(toto)
   #a.execute( 'Select', objects=[ x[ 'ana_object' ] for x in graph.vertices() ] )
   a.execute( 'SetControl', windows=[win], control='BundlesSelectionControl' )
   action = win.view().controlSwitch().getAction( 'BundlesSelectionAction' )
   action.secondaryView = win2
 
-  return [mesh, toto, anaclusters, tex, win, win2]
+  return [mesh, graph_list, anaclusters, big_textured_mesh, win, win2]
 
 def execution( self, context ):
     return mainThreadActions().call( self.execution_mainthread, context )
