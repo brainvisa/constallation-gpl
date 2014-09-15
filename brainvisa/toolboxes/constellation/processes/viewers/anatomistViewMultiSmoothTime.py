7c5b8a7bf0fc2109ae4298f172a127e34ab2ed0a
denisri <denis.riviere.gm@gmail.com>
2014-09-15 10:32:00 +0000

    use qt instead of gtk to find out screen properties
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@63532 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/viewers/anatomistViewMultiSmoothTime.py
+++ brainvisa/toolboxes/constellation/processes/viewers/anatomistViewMultiSmoothTime.py
@@ -29,29 +29,50 @@ def initialization(self):
     self.k_time = 3 
     
 
+def get_screen_config():
+    desktop = QtGui.qApp.desktop()
+    print "desktop size: %d x %d" % (desktop.width(), desktop.height())
+    monitors = []
+    nmons = desktop.screenCount()
+    print "there are %d monitors" % nmons
+    for m in range(nmons):
+        #mg = desktop.screen(m)
+        mg = desktop.availableGeometry(m)
+        print "monitor %d: %d, %d, %d x %d" % (m, mg.x(), mg.y(), mg.width(), mg.height())
+        monitors.append((mg.x(), mg.y(), mg.width(), mg.height()))
+
+    # current monitor
+    curmon = desktop.screenNumber(QtGui.QCursor.pos())
+    x, y, width, height = monitors[curmon]
+    print "monitor %d: %d x %d (current)" % (curmon,width,height)
+    return (curmon, monitors[curmon])
+
+    #window = gtk.Window()
+    ## the screen contains all monitors
+    #screen = window.get_screen()
+    #print "screen size: %d x %d" % (gtk.gdk.screen_width(),gtk.gdk.screen_height())
+    ## collect data about each monitor
+    #monitors = []
+    #nmons = screen.get_n_monitors()
+    #print "there are %d monitors" % nmons
+    #for m in range(nmons):
+        #mg = screen.get_monitor_geometry(m)
+        #print "monitor %d: %d x %d" % (m,mg.width,mg.height)
+        #monitors.append(mg)
+
+    ## current monitor
+    #curmon = screen.get_monitor_at_window(screen.get_active_window())
+    #x, y, width, height = monitors[curmon]
+    #print "monitor %d: %d x %d (current)" % (curmon,width,height)
+
 def execution(self, context):
     nb_tex = len(self.clustering_texture)
     a = ana.Anatomist()
-    
-    window = gtk.Window()
-    # the screen contains all monitors
-    screen = window.get_screen()
-    print "screen size: %d x %d" % (gtk.gdk.screen_width(),gtk.gdk.screen_height())
-    # collect data about each monitor
-    monitors = []
-    nmons = screen.get_n_monitors()
-    print "there are %d monitors" % nmons
-    for m in range(nmons):
-        mg = screen.get_monitor_geometry(m)
-        print "monitor %d: %d x %d" % (m,mg.width,mg.height)
-        monitors.append(mg)
 
-    # current monitor
-    curmon = screen.get_monitor_at_window(screen.get_active_window())
-    x, y, width, height = monitors[curmon]
-    print "monitor %d: %d x %d (current)" % (curmon,width,height)  
+    curmon, monitor = mainThreadActions().call(get_screen_config)
+
     block = a.createWindowsBlock(nbCols=5, nbRows=4)
-    
+
     w = []
     t = []
     for i in xrange(nb_tex):
