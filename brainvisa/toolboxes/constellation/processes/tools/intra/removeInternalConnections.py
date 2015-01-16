9addfb50029a281145b5fae976be7efe0105085e
lefranc <lefranc@bioproj.invalid>
2015-01-16 17:24:23 +0000

    remove "white_mesh" input and "patch" (not necessary) added new input: "keep_internal_connections" as Boolean
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@65284 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/tools/intra/removeInternalConnections.py
+++ brainvisa/toolboxes/constellation/processes/tools/intra/removeInternalConnections.py
@@ -1,26 +1,15 @@
-############################################################################
-#  This software and supporting documentation are distributed by
-#      CEA/NeuroSpin, Batiment 145,
-#      91191 Gif-sur-Yvette cedex
-#      France
-# This software is governed by the CeCILL license version 2 under
-# French law and abiding by the rules of distribution of free software.
-# You can  use, modify and/or redistribute the software under the
-# terms of the CeCILL license version 2 as circulated by CEA, CNRS
-# and INRIA at the following URL "http://www.cecill.info".
-############################################################################
+###############################################################################
+# This software and supporting documentation are distributed by CEA/NeuroSpin,
+# Batiment 145, 91191 Gif-sur-Yvette cedex, France. This software is governed
+# by the CeCILL license version 2 under French law and abiding by the rules of
+# distribution of free software. You can  use, modify and/or redistribute the
+# software under the terms of the CeCILL license version 2 as circulated by
+# CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
+###############################################################################
 
 # BrainVisa module
 from brainvisa.processes import *
-
-# System module
-import warnings
-
-# Plot aims module
-try:
-    from soma import aims
-except:
-    warnings.warn("Please make sure that aims module is installed.")
+from soma.path import find_in_path
 
 name = "Remove Internal Connections"
 userLevel = 2
@@ -29,48 +18,36 @@ userLevel = 2
 signature = Signature(
     "patch_connectivity_profile", ReadDiskItem(
         "Gyrus Connectivity Profile", "Aims texture formats"),
-    "white_mesh", ReadDiskItem("Mesh", "Aims mesh formats"),
     "gyri_texture", ReadDiskItem("Label Texture", "Aims texture formats"),
-    "patch", Integer(),
     "normed_connectivity_profile", WriteDiskItem(
         "Normed Connectivity Profile", "Aims texture formats"),
+    "keep_internal_connections", Boolean(),
 )
 
 
 def initialization(self):
     """Provides default values and link of parameters
     """
-    self.linkParameters("white_mesh", "patch_connectivity_profile")
+    self.keep_internal_connections = False
     self.linkParameters(
         "normed_connectivity_profile", "patch_connectivity_profile")
 
-    self.setOptional("patch")
-    self.signature["white_mesh"].userLevel = 2
-    
 
 def execution(self, context):
-    """remove internals connections of patch
     """
-    # provides the patch name
-    if self.patch is not None:
-        patch = self.patch  # keep internal connections, put 0
+    STEP 1/2: Remove internals connections of patch.
+    STEP 2/2: The profile is normalized. 
+    """
+    args = []
+    args += ["-p", self.patch_connectivity_profile,
+             "-g", self.gyri_texture,
+             "-n", self.normed_connectivity_profile]
+
+    if not self.keep_internal_connections:
+        args += ["-q", self.keep_internal_connections]
     else:
-        patch = os.path.basename(
-            os.path.dirname(os.path.dirname(os.path.dirname(
-                self.patch_connectivity_profile.fullPath()))))
-        patch = patch.strip("G")
+        args += ["-c", self.keep_internal_connections]
 
-    # remove internals connections
-    mask = aims.read(self.gyri_texture.fullPath())
-    pcp = aims.read(self.patch_connectivity_profile.fullPath())
-    maskarr = mask[0].arraydata()
-    pcparr = pcp[0].arraydata()
-    for i in xrange(pcp[0].nItem()):
-        pcparr[maskarr == int(patch)] = 0
-
-    # the profile is normalized
-    dividende_coef = pcparr.max()
-    if dividende_coef > 0:
-        z = 1. / dividende_coef
-        pcparr *= z
-    aims.write(pcp, self.normed_connectivity_profile.fullPath())
+    context.system(sys.executable,
+                   find_in_path("constelRemoveInternalConnections.py"),
+                   *args)
