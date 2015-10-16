7e01388673f165f77e20b6053db8dd604fc14261
lefranc <lefranc@bioproj.invalid>
2015-10-16 15:35:41 +0000

    the inputs "ROI" and "ROIs_nomenclature" have been added in the signature of the process. The name of the ROI is determined from the file of fibertracts. The number of the ROI is determined from the ROis nomenclature. + link parameters
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@69596 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/tools/intra/sumSparseMatrix.py
+++ brainvisa/toolboxes/constellation/processes/tools/intra/sumSparseMatrix.py
@@ -7,64 +7,147 @@
 # CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
 ###############################################################################
 
-# Axon python API modules
-from brainvisa.processes import *
+"""
+This script does the following:
+* defines a Brainvisa process
+    - the parameters of a process (Signature),
+    - the parameters initialization
+    - the linked parameters
+* this process
+
+Main dependencies: Axon python API, Soma-base, constel
+
+Author: Sandrine Lefranc, 2015
+"""
+
+#----------------------------Imports-------------------------------------------
+
+
+# axon python API module
+from brainvisa.processes import Signature, ReadDiskItem, Float, String, \
+    WriteDiskItem, ValidationError
+
+# soma-base module
 from soma.path import find_in_path
 
 # constel module
-from constel.lib.texturetools import identify_patch_number
+try:
+    from constel.lib.utils.files import select_ROI_number
+except:
+    pass
 
-# Plot aims modules
+
 def validation():
-    """This function is executed at setup when the process is loaded. 
-
-    It checks some conditions for the process to be available.
+    """This function is executed at BrainVisa startup when the process is
+    loaded. It checks some conditions for the process to be available.
     """
     if not find_in_path("AimsSumSparseMatrix") \
-            or not find_in_path("AimsSparseMatrixSmoothing"):
+            or not find_in_path("AimsSparseMatrixSmoothing"):  # checks command
         raise ValidationError(
-            "Please make sure that constel module is installed.")
+            "AimsSumSparseMatrix or AimsSparseMatrixSmoothing is not contained"
+            "in PATH environnement variable."
+            "Please make sure that aims is installed.")
+
+
+#----------------------------Header--------------------------------------------
+
 
 name = "Smoothing Matrix"
 userLevel = 2
 
-# Argument declaration
 signature = Signature(
+    # --inputs--
     "matrix_of_fibers_near_cortex", ReadDiskItem(
-        "Connectivity Matrix Fibers Near Cortex", "Matrix sparse"),
+        "Connectivity Matrix", "Matrix sparse",
+        requiredAttributes={"ends_labelled": "both",
+                            "reduced": "No",
+                            "dense": "No",
+                            "intersubject": "No"}),
     "matrix_of_distant_fibers", ReadDiskItem(
-        "Connectivity Matrix Outside Fibers Of Cortex", "Matrix sparse"),
-    "gyri_texture", ReadDiskItem("Label Texture", "Aims texture formats"),
-    "white_mesh", ReadDiskItem("Mesh", "Aims mesh formats"),
+        "Connectivity Matrix", "Matrix sparse",
+        requiredAttributes={"ends_labelled": "single",
+                            "reduced": "No",
+                            "dense": "No",
+                            "intersubject": "No"}),
+    "ROIs_nomenclature", ReadDiskItem("Nomenclature ROIs File", "Text File"),
+    "ROI", String(),
+    "gyri_texture", ReadDiskItem(
+        "ROI Texture", "Aims texture formats",
+        requiredAttributes={"side": "both", "vertex_corr": "Yes"}),
+    "white_mesh", ReadDiskItem(
+        "White Mesh", "Aims mesh formats",
+        requiredAttributes={"side": "both", "vertex_corr": "Yes"}),
     "smoothing", Float(),
+
+    # --outputs--
     "complete_connectivity_matrix", WriteDiskItem(
-        "Gyrus Connectivity Matrix", "Matrix sparse"),
+        "Connectivity Matrix", "Matrix sparse",
+        requiredAttributes={"ends_labelled": "mixed",
+                            "reduced": "No",
+                            "dense": "No",
+                            "intersubject": "No"}),
 )
 
 
-# Default values
+#----------------------------Functions-----------------------------------------
+
+
 def initialization(self):
     """Provides default values and link of parameters"""
-    self.smoothing = 3.0
 
-    def linkSmooth(self, dummy):
-        if self.matrix_of_distant_fibers is not None:
-            attrs = dict(self.matrix_of_distant_fibers.hierarchyAttributes())
-            attrs["smoothing"] = str(self.smoothing)
+    # default values
+    self.smoothing = 3.0
+    self.ROIs_nomenclature = self.signature["ROIs_nomenclature"].findValue({})
+
+    def link_matrix2ROI(self, dummy):
+        """Define the attribut 'gyrus' from fibertracts pattern for the
+        signature 'ROI'.
+        """
+        if self.matrix_of_fibers_near_cortex is not None:
+            s = str(self.matrix_of_fibers_near_cortex.get("gyrus"))
+            name = self.signature["ROI"].findValue(s)
+        return name
+
+    def link_matrix(sef, dummy):
+        """
+        """
+        if self.matrix_of_fibers_near_cortex is not None:
+            attrs = dict(
+                self.matrix_of_fibers_near_cortex.hierarchyAttributes())
+            attrs["smallerlength1"] = None
+            attrs["greaterlength1"] = None
             filename = self.signature[
-                "complete_connectivity_matrix"].findValue(attrs)
+                "matrix_of_distant_fibers"].findValue(attrs)
             return filename
 
-    self.linkParameters(
-        "matrix_of_distant_fibers", "matrix_of_fibers_near_cortex")
+    def link_smooth(self, dummy):
+        """
+        """
+        if (self.matrix_of_distant_fibers and self.smoothing) is not None:
+            attrs = dict(self.matrix_of_distant_fibers.hierarchyAttributes())
+            attrs["smoothing"] = str(self.smoothing)
+            attrs["smallerlength2"] = None
+            attrs["greaterlength2"] = None
+            filename = self.signature[
+                "complete_connectivity_matrix"].findValue(attrs)
+            print attrs
+            return filename
+
+    # link of parameters for autocompletion
+    self.linkParameters("ROI", "matrix_of_fibers_near_cortex", link_matrix2ROI)
+    self.linkParameters("matrix_of_fibers_near_cortex",
+                        "matrix_of_distant_fibers", link_matrix)
     self.linkParameters("complete_connectivity_matrix",
-                        ("matrix_of_distant_fibers", "smoothing"), linkSmooth)
+                        ("matrix_of_distant_fibers", "smoothing"), link_smooth)
+
+
+#----------------------------Main program--------------------------------------
 
 
 def execution(self, context):
     """Sum of two matrices and smoothing"""
-    # provides the patch name
-    patch = identify_patch_number(self.matrix_of_fibers_near_cortex.fullPath())
+    # selects the ROI label corresponding to ROI name
+    ROIlabel = select_ROI_number(self.ROIs_nomenclature.fullPath(), self.ROI)
 
     # sum matrix
     context.system("AimsSumSparseMatrix",
@@ -79,4 +162,4 @@ def execution(self, context):
                    "-o", self.complete_connectivity_matrix,
                    "-s", self.smoothing,
                    "-l", self.gyri_texture,
-                   "-p", patch)
+                   "-p", ROIlabel)
