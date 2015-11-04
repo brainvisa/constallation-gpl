c690ddba23d0f96021413a89c02cb297214359af
lefranc <lefranc@bioproj.invalid>
2015-11-04 09:59:00 +0000

    rename a file (new name: nomenclature_desikan_freesurfer)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/trunk@69958 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/hierarchies/shared/constellation.py
+++ brainvisa/toolboxes/constellation/hierarchies/shared/constellation.py
@@ -1,7 +1,7 @@
 include('base')
 
 insert("nomenclature/translation",
-     "freesurfer_desikan_gyri", SetType("Nomenclature ROIs File"),
+     "nomenclature_desikan_freesurfer", SetType("Nomenclature ROIs File"),
      "nomenclature_destrieux_freesurfer", SetType("Nomenclature ROIs File")
 )
 
