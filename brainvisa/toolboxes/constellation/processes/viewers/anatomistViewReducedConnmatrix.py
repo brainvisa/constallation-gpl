4790e3eb2327dda72269a43f65acffd5850f4990
lefranc <lefranc@bioproj.invalid>
2016-01-08 12:35:13 +0000

    anatomistViewReducedConnmatrix.py: code cleanup (pep8) and some small improvements + doc
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/branches/bug_fix@70998 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/constellation/processes/viewers/anatomistViewReducedConnmatrix.py
+++ brainvisa/toolboxes/constellation/processes/viewers/anatomistViewReducedConnmatrix.py
@@ -9,8 +9,15 @@
 
 """
 This script does the following:
-*
-*
+* Connectivity profiles of a specific cortical element
+* cortical element = vertex or region
+* Connectivity profile on a textured mesh of the cortical surface in an
+  iteractive way, by electing either a vertex or a cortical region.
+      - option 1: vertex of ROI --> vertices of cortex,
+      - option 2: vertex of cortex --> vertices of ROI,
+      - option 3: cluster of ROI --> vertices of cortex,
+      - option 4: vertex of ROI --> basins
+      - option 5: cluster of ROI --> basins
 
 Main dependencies:
 
@@ -21,70 +28,87 @@ Author: Sandrine Lefranc, 2015
 
 
 # axon python API module
-from brainvisa.processes import *
+from brainvisa.processes import Signature, ReadDiskItem,
 
 try:
     from brainvisa import anatomist as ana
 except:
     pass
 
+
 def validation():
     try:
         from brainvisa import anatomist as ana
     except:
-        raise ValidationError(_t_('Anatomist not available'))
+        raise ValidationError(_t_("Anatomist not available"))
 
 
 #----------------------------Header--------------------------------------------
 
 
-name = 'Anatomist view reduced connectivity matrix'
-roles = ('viewer', )
+name = "Anatomist Show Connectivity Profiles of a Specific Cortical Element"
+roles = ("viewer", )
 userLevel = 0
 
 signature = Signature(
-    'connectivity_matrix',
-        ReadDiskItem('connectivity matrix', 'aims readable volume formats',
-                     requiredAttributes={"ends_labelled":"mixed",
-                                         "reduced":"Yes",
-                                         "dense":"No",
-                                         "intersubject":"Yes"}),
-    'white_mesh', ReadDiskItem('White Mesh', 'anatomist mesh formats',
-                               requiredAttributes={"side":"both",
-                                                   "vertex_corr":"Yes"}),
-    'gyrus_texture', 
-        ReadDiskItem('ROI texture', 'anatomist texture formats',
-                     requiredAttributes={"side":"both",
-                                         "vertex_corr":"Yes"}),
-    'basins_texture',
-        ReadDiskItem('Label texture', 'anatomist texture formats'), )
+    "connectivity_matrix", ReadDiskItem(
+        "connectivity matrix", "aims readable volume formats",
+        requiredAttributes={"ends_labelled": "mixed",
+                            "reduced": "Yes",
+                            "dense": "No",
+                            "intersubject": "Yes"}),
+    "white_mesh", ReadDiskItem("White Mesh", "anatomist mesh formats",
+                               requiredAttributes={"side": "both",
+                                                   "vertex_corr": "Yes"}),
+    "gyrus_texture",
+        ReadDiskItem("ROI texture", "anatomist texture formats",
+                     requiredAttributes={"side": "both",
+                                         "vertex_corr": "Yes"}),
+    "basins_texture",
+        ReadDiskItem("Label texture", "anatomist texture formats"), )
 
 
 #----------------------------Function------------------------------------------
 
 
 def initialization(self):
-    self.linkParameters('white_mesh', 'connectivity_matrix')
-    self.linkParameters('gyrus_texture', 'connectivity_matrix')
-    self.linkParameters('basins_texture', 'connectivity_matrix')
+    self.linkParameters("white_mesh", "connectivity_matrix")
+    self.linkParameters("gyrus_texture", "connectivity_matrix")
+    self.linkParameters("basins_texture", "connectivity_matrix")
 
 
 #----------------------------Main program--------------------------------------
 def execution(self, context):
+    """
+    """
+    # instance of anatomist
     a = ana.Anatomist()
+
+    # load an object from a file
     mesh = a.loadObject(self.white_mesh)
     patch = a.loadObject(self.gyrus_texture)
     sparse = a.loadObject(self.connectivity_matrix)
     basins = a.loadObject(self.basins_texture)
+
+    # create a fusionned multi object that contains all given objects
     conn = a.fusionObjects(
-        [mesh, patch, sparse, basins], method='ConnectivityMatrixFusionMethod')
+        [mesh, patch, sparse, basins], method="ConnectivityMatrixFusionMethod")
     if conn is None:
         raise ValueError(
-            'could not fusion objects - matrix, mesh and ' \
-            'labels texture may not correspond.')
-    win = a.createWindow('3D')
-    a.execute('WindowConfig', windows=[win], light={'background' : [0.,0.,0.,0.]})
+            "could not fusion objects - matrix, mesh and "
+            "labels texture may not correspond.")
+
+    # create a new window and opens it
+    win = a.createWindow("3D")
+
+    # executes a command in anatomist application
+    a.execute(
+        "WindowConfig", windows=[win], light={"background": [0., 0., 0., 0.]})
+
+    # add objects in windows
     win.addObjects(conn)
-    win.setControl('ConnectivityMatrixControl')
+
+    # changes the selected button in windows menu.
+    win.setControl("ConnectivityMatrixControl")
 
     return [win, conn, basins]
