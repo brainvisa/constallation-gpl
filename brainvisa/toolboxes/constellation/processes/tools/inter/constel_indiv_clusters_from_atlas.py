40702e31ac663ca9ce558198180d4ab20f3f567b
lefranc <lefranc@bioproj.invalid>
2016-11-29 12:35:21 +0000

    tools/inter: add new process to compute the nearest neighbour profiles. (link of parameters are not done)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-gpl/branches/bug_fix@75607 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ brainvisa/toolboxes/constellation/processes/tools/inter/constel_indiv_clusters_from_atlas.py
@@ -0,0 +1,103 @@
+###############################################################################
+# This software and supporting documentation are distributed by CEA/NeuroSpin,
+# Batiment 145, 91191 Gif-sur-Yvette cedex, France. This software is governed
+# by the CeCILL license version 2 under French law and abiding by the rules of
+# distribution of free software. You can  use, modify and/or redistribute the
+# software under the terms of the CeCILL license version 2 as circulated by
+# CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
+###############################################################################
+
+"""
+This script does the following:
+* defines a Brainvisa process
+    - the signature of the inputs/ouputs,
+    - the initialization (by default) of the inputs,
+    - the interlinkages between inputs/outputs.
+* executes the command 'constel_clusters_from_atlas.py':
+
+Main dependencies: axon python API, soma, constel
+
+Author: Sandrine Lefranc, 2016
+"""
+
+#----------------------------Imports-------------------------------------------
+
+
+# axon python API module
+from brainvisa.processes import Signature
+from brainvisa.processes import ReadDiskItem
+from brainvisa.processes import WriteDiskItem
+from brainvisa.processes import ValidationError
+
+# soma module
+from soma.path import find_in_path
+
+
+def validation():
+    """This function is executed at BrainVisa startup when the process is
+    loaded. It checks some conditions for the process to be available.
+    """
+    if not find_in_path("constel_clusters_from_atlas.py"):
+        raise ValidationError(
+            "Please make sure that constel module is installed.")
+
+
+#----------------------------Header--------------------------------------------
+
+
+name = "Individual clusters from atlas"
+userLevel = 2
+
+signature = Signature(
+    # --inputs--
+    "individual_matrix", ReadDiskItem(
+        "Connectivity Matrix", "Aims matrix formats",
+        requiredAttributes={"ends_labelled": "mixed",
+                            "reduced": "Yes",
+                            "dense": "No",
+                            "intersubject": "Yes"}),
+    "atlas_matrix", ReadDiskItem(
+        "Connectivity Matrix", "Aims matrix formats",
+        requiredAttributes={"ends_labelled": "mixed",
+                            "reduced": "Yes",
+                            "dense": "No",
+                            "intersubject": "Yes"}),
+    "group_clustering", ReadDiskItem(
+        "Connectivity ROI Texture", "Aims texture formats",
+        requiredAttributes={"roi_autodetect": "No",
+                            "roi_filtered": "No",
+                            "averaged": "No",
+                            "intersubject": "Yes",
+                            "step_time": "Yes"}),
+
+
+    # --outputs--
+    "individual_clustering", WriteDiskItem(
+        "Connectivity ROI Texture", "Aims texture formats",
+        requiredAttributes={"roi_autodetect": "No",
+                            "roi_filtered": "No",
+                            "averaged": "No",
+                            "intersubject": "Yes",
+                            "step_time": "Yes"}),
+)
+
+
+#----------------------------Functions-----------------------------------------
+
+
+def initialization(self):
+    """Provides link of parameters"""
+    pass
+
+
+#----------------------------Main program--------------------------------------
+
+
+def execution(self, context):
+    """Run the command 'constel_clusters_from_atlas'.
+    """
+    context.pythonSystem("constel_clusters_from_atlas.py",
+                         self.individual_matrix,
+                         self.atlas_matrix,
+                         self.group_clustering,
+                         self.individual_clustering)
