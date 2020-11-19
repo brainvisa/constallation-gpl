5bda8a87449f412b97552344610523c4e29fa6ca
clanglet <clanglet@bioproj.invalid>
2020-11-19 13:49:35 +0000

    Added process for selection of optimal clustering according to a criterion
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/constellation/constellation-gpl/branches/bug_fix@87025 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ brainvisa/toolboxes/constellation/processes/miscellaneous/constel_optimal_clustering.py
@@ -0,0 +1,94 @@
+###############################################################################
+# This software and supporting documentation are distributed by CEA/NeuroSpin,
+# Batiment 145, 91191 Gif-sur-Yvette cedex, France. This software is governed
+# by the CeCILL license version 2 under French law and abiding by the rules of
+# distribution of free software. You can  use, modify and/or redistribute the
+# software under the terms of the CeCILL license version 2 as circulated by
+# CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
+###############################################################################
+
+from brainvisa.processes import Signature
+from brainvisa.processes import ReadDiskItem
+from brainvisa.processes import WriteDiskItem
+from brainvisa.processes import OpenChoice
+
+# soma module
+from soma.path import find_in_path
+
+# ---------------------------Imports-------------------------------------------
+
+
+def validation(self):
+    """This function is executed at BrainVisa startup when the process is
+    loaded. It checks some conditions for the process to be available.
+    """
+    if not find_in_path("constel_select_optimal_clustering.py"):
+        raise ValidationError(
+            "Please make sure that constel module is installed.")
+
+# ---------------------------Header--------------------------------------------
+
+
+name = "Constellation Optimal Clustering"
+userLevel = 1
+
+signature = Signature(
+    # --inputs
+    "individual_clustering", ReadDiskItem("Connectivity ROI Texture",
+                                          "Aims texture formats",
+                                          requiredAttributes={
+                                            "roi_autodetect": "no",
+                                            "roi_filtered": "no",
+                                            "intersubject": "yes",
+                                            "step_time": "yes",
+                                            "measure": "no"},
+                                          section="Clustering inputs"),
+    "atlas_silhouette", ReadDiskItem("Clustering Silhouette",
+                                     "JSON file",
+                                     section="Atlas inputs"),
+
+    # outputs
+    "optimal_clustering", WriteDiskItem("Connectivity ROI Texture",
+                                        "Aims texture formats",
+                                        requiredAttributes={
+                                            "roi_autodetect": "no",
+                                            "roi_filtered": "no",
+                                            "intersubject": "yes",
+                                            "step_time": "yes",
+                                            "measure": "no",
+                                            "optimal": "silhouette"},
+                                        section="Optimal result"),
+)
+
+# ---------------------------Functions-----------------------------------------
+
+
+def initialization(self):
+    """
+    """
+
+    def link_optimal_cluster(self, dummy):
+        """
+        """
+        if self.individual_clustering:
+            match = dict(self.individual_clustering.hierarchyAttributes())
+            return self.signature["optimal_clustering"].findValue(match)
+
+    self.linkParameters("optimal_clustering",
+                        "individual_clustering",
+                        link_optimal_cluster)
+
+
+def execution(self, context):
+    """Run the command 'constel_select_optimal_clustering.py'.
+    """
+
+    region = self.individual_clustering.hierarchyAttributes()['gyrus']
+
+    cmd = ["constel_select_optimal_clustering.py",
+           self.individual_clustering,
+           self.atlas_silhouette,
+           region,
+           self.optimal_clustering]
+
+    context.pythonSystem(*cmd)
