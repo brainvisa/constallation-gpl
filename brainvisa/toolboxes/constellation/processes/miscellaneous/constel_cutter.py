009494a967247660ded3f2d322f17b027fdda63a
clanglet <clement.langlet@cea.fr>
2021-11-16 15:05:47 +0100

    Updated cutter to support multiple regions cut

--- brainvisa/toolboxes/constellation/processes/miscellaneous/constel_cutter.py
+++ brainvisa/toolboxes/constellation/processes/miscellaneous/constel_cutter.py
@@ -13,6 +13,7 @@ from brainvisa.processes import ReadDiskItem
 from brainvisa.processes import WriteDiskItem
 from brainvisa.processes import OpenChoice
 from brainvisa.processes import Choice
+from brainvisa.processes import ListOf
 from brainvisa.processes import ValidationError
 from brainvisa.processes import Boolean
 
@@ -31,6 +32,7 @@ def validation(self):
             "Please make sure that constel module is installed.")
     try:
         from constel.lib.utils.filetools import select_ROI_number
+        from constel.lib.utils.filetools import read_nomenclature_file
     except ImportError:
         raise ValidationError(
             "Please make sure that constel module is installed.")
@@ -49,18 +51,18 @@ signature = Signature(
     "original_texture", ReadDiskItem("Connectivity ROI Texture",
                                      "Aims texture formats",
                                      section="Inputs"),
-    "region", OpenChoice(section="Inputs"),
+    "regions", ListOf(OpenChoice(), section="Inputs"),
 
     # outputs
     "cut_texture", WriteDiskItem("Connectivity ROI Texture",
                                  "Aims texture formats",
                                  section="Outputs"),
 
-    "cut_region", WriteDiskItem("Connectivity ROI Texture",
-                                "Aims texture formats",
-                                section="Outputs"),
+    "cut_regions", WriteDiskItem("Connectivity ROI Texture",
+                                 "Aims texture formats",
+                                 section="Outputs"),
     "keep_cut_texture", Boolean(section="Options"),
-    "keep_cut_region", Boolean(section="Options"),
+    "keep_cut_regions", Boolean(section="Options"),
 )
 
 # ---------------------------Functions-----------------------------------------
@@ -69,39 +71,26 @@ signature = Signature(
 def initialization(self):
     """
     """
+    from constel.lib.utils.filetools import read_nomenclature_file
+
     self.keep_cut_texture = True
-    self.keep_cut_region = True
+    self.keep_cut_regions = True
 
-    def reset_label(self, dummy):
-        """Read and/or reset the region parameter.
-
-        This callback reads the labels nomenclature and proposes them in the
-        signature 'region' of process.
-        It also resets the region parameter to default state after
-        the nomenclature changes.
+    def link_regions(self, dummy):
         """
-        from constel.lib.utils.filetools import read_nomenclature_file
-        current = self.region
-        self.setValue("region", current, True)
+        """
         if self.regions_nomenclature is not None:
-            s = [("Select a region in this list", None)]
-            # temporarily set a value which will remain valid
-            self.region = s[0][1]
+            self.keep_regions = None
+            s = []
             s += read_nomenclature_file(
                 self.regions_nomenclature.fullPath(), mode=2)
-            self.signature["region"].setChoices(*s)
-            if isinstance(self.signature["region"], OpenChoice):
-                self.signature["region"] = Choice(*s)
-                self.changeSignature(self.signature)
-            if current not in s:
-                self.setValue("region", s[0][1], True)
-            else:
-                self.setValue("region", current, True)
+            self.signature["regions"] = ListOf(Choice(*s), section="Inputs")
+            self.changeSignature(self.signature)
 
     def link_cut_texture(self, dummy):
         """
         """
-        if self.region and self.original_texture:
+        if self.regions and self.original_texture:
             ext = os.path.splitext(self.original_texture.fullPath())[1]
             filename = self.original_texture.fullName() + '_cut' + ext
             return filename
@@ -109,22 +98,24 @@ def initialization(self):
     def link_cut_region(self, dummy):
         """
         """
-        if self.region and self.original_texture:
+        if self.regions and self.original_texture:
             ext = os.path.splitext(self.original_texture.fullPath())[1]
-            region = '_' + self.region
-            filename = self.original_texture.fullName() + '_cut' + region + ext
+            filename = self.original_texture.fullName() + '_cut'
+            for region in self.regions:
+                filename += '-' + region
+            filename += ext
             return filename
 
     self.linkParameters(None,
                         "regions_nomenclature",
-                        reset_label)
+                        link_regions)
 
     self.linkParameters("cut_texture",
-                        ["region", "original_texture"],
+                        ["regions", "original_texture"],
                         link_cut_texture)
 
-    self.linkParameters("cut_region",
-                        ["region", "original_texture"],
+    self.linkParameters("cut_regions",
+                        ["regions", "original_texture"],
                         link_cut_region)
 
 
@@ -133,19 +124,23 @@ def execution(self, context):
     """
     from constel.lib.utils.filetools import select_ROI_number
 
-    # Selects the label number corresponding to label name
-    label = select_ROI_number(self.regions_nomenclature.fullPath(),
-                              self.region)
+    # Selects the label numbers corresponding to label names
+    labels = []
+    for region in self.regions:
+        label = select_ROI_number(self.regions_nomenclature.fullPath(),
+                                  region)
+        labels.append(int(label))
+
     cmd = ["constel_cut_region.py",
            self.original_texture,
-           label,
+           labels,
            self.cut_texture,
-           self.cut_region]
+           self.cut_regions]
 
     context.pythonSystem(*cmd)
 
     if not self.keep_cut_texture:
         self.cut_texture.eraseFiles()
 
-    if not self.keep_cut_region:
-        self.cut_region.eraseFiles()
+    if not self.keep_cut_regions:
+        self.cut_regions.eraseFiles()
